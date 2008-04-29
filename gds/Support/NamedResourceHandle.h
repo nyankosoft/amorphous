71eeffc9ec8f077ac1ffc40737f352bdc040357d
Takashi Kumagai <Takashi Kumagai@b80c8c75-5753-0c4f-8f2c-6ef837923b1e>
2008-04-29 04:41:25 +0000

    added: gds/Support/NamedResourceHandle.h

---
+++ gds/Support/NamedResourceHandle.h
@@ -0,0 +1,57 @@
+#ifndef  __NamedResourceHandle_H__
+#define  __NamedResourceHandle_H__
+
+
+#include "Support/Serialization/Serialization.h"
+using namespace GameLib1::Serialization;
+
+#include <string>
+
+
+class CNamedResourceHandle : public IArchiveObjectBase
+{
+protected:
+
+	/// name of the resource
+	std::string m_Name;
+
+	/// index to the resource
+	int m_iIndex;
+
+public:
+
+	CNamedResourceHandle() : m_iIndex(INVALID_INDEX) {}
+
+	CNamedResourceHandle( const std::string& name ) : m_Name(name), m_iIndex(UNINITIALIZED) {}
+
+//	inline int GetIndex() { return m_iIndex; }
+
+	inline const std::string &GetName() const { return m_Name; }
+
+	inline void SetName( const std::string& name )
+	{
+		m_Name = name;
+		m_iIndex = UNINITIALIZED;	// clear the index when a new name is set
+	}
+
+	inline virtual void Serialize( IArchive& ar, const unsigned int version )
+	{
+		std::string name;
+		if( ar.GetMode() == IArchive::MODE_INPUT )
+		{
+			ar & name;
+			SetName( name );
+		}
+		else
+		{
+			name = GetName();
+			ar & name;
+		}
+	}
+
+	enum eSpecialIndexVal { INVALID_INDEX = -1, UNINITIALIZED = -2 };
+};
+
+
+
+#endif		/*  __NamedResourceHandleR_H__  */
