4305c0ef957f524605caff1364db4b6ac2e80baf
Takashi Kumagai <Takashi Kumagai@b80c8c75-5753-0c4f-8f2c-6ef837923b1e>
2008-04-29 04:07:06 +0000

    added: draft of AssaultRecord.h

---
+++ gds/GameCommon/AssaultRecord.h
@@ -0,0 +1,80 @@
+#ifndef __ASSAULTRECORD_H__
+#define __ASSAULTRECORD_H__
+
+
+#include "Support/Serialization/Serialization.h"
+#include "Support/Serialization/ArchiveObjectFactory.h"
+using namespace GameLib1::Serialization;
+
+
+/**
+ * holds destroyed entities records
+ * name & type are copied from KillReport
+ * some information exist in KillReport is omitted
+ */
+class KillRecord : public IArchiveObjectBase
+{
+public:
+	std::string name	/// copied from KillReport::entity_name
+	int num_destroyed;	/// how many entities of this type has been destroyed
+
+	int type;	/// type of the destroyed target
+
+public:
+
+	KillRecord() : num_destroyed(0), type(0) {}
+
+	virtual void Serialize( IArchive& ar, const unsigned int version )
+	{
+		ar & name & num_destroyed;
+		ar & type;
+	}
+};
+
+
+class CAssaultRecord : public IArchiveObjectBase
+{
+	std::vector<KillRecord> m_vecKillRecord;
+
+public:
+
+	void AddRecord( const CombatReocrd& combat_record )
+	{
+		size_t i, num = m_vecKillRecord.size();
+		for( i=0; i<num; i++ )
+		{
+			const KillReport& rep = combat_record.m_vecKillReport[i];
+			if( rep.entity_name == rec.name )
+			{
+				rec.num_destroyed++;
+				return;
+			}
+		}
+
+		// create a new item
+		m_vec.push_back( KillRecord() );
+		m_vec.back().num_destroyed = 1;
+		m_vec.back().name = rep.entity_name;
+	}
+
+	void Serialize( IArchive& ar, const unsigned int version )
+	{
+		ar & m_vecKillRecord;
+	}
+
+};
+
+
+/*
+class CSaveObjectBase : public IArchiveObjectBase
+{
+public:
+
+	CSaveObjectBase() { SaveObjectManager.Regsiter(this); }
+
+	~CSaveObjectBase() { SaveObjectManager.Release(this); }
+};
+*/
+
+
+#endif /* __ASSAULTRECORD_H__ */
