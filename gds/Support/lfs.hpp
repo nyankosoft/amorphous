1e606b37d28e58229189e1fe6cc194dbbcffac23
Takashi Kumagai <Takashi Kumagai@b80c8c75-5753-0c4f-8f2c-6ef837923b1e>
2009-02-07 02:39:32 +0000

    added: a draft of Support/lfs.hpp   - a future replacement of Support/fnop.hpp

---
+++ gds/Support/lfs.hpp
@@ -0,0 +1,346 @@
+#ifndef __LFS_HPP__
+#define __LFS_HPP__
+
+
+#include <sys/stat.h>
+#include <errno.h>
+#include <string>
+#include <vector>
+
+#ifdef _MSC_VER
+#include <direct.h>
+#include <assert.h>
+#define getcwd _getcwd
+#define chdir  _chdir
+#else /* _MSC_VER */
+#include <unistd.h>
+#endif /* _MSC_VER */
+
+
+/**
+ lfs.hpp
+   - Various Light FileSystem operations
+*/
+
+// NOTE:
+// using chdir(), getcwd() under Win32 causes LNK2005 error
+// use _chdir(), _getcwd() instead
+
+namespace lfs
+{
+
+#define LFS_MAX_PATH 1024
+
+
+inline void change_ext( std::string& target_filename, const std::string& dest_ext )
+{
+	size_t pos = target_filename.rfind( "." );
+
+	if( pos == std::string::npos )
+		return;
+	else
+        target_filename = target_filename.substr( 0, pos + 1 ) + dest_ext;
+}
+
+
+/// returns the extension(suffix) of the filename
+inline std::string get_ext( const std::string& filename )
+{
+	size_t pos = filename.rfind( "." );
+
+	if( pos == std::string::npos )
+		return std::string("");
+	else
+        return filename.substr( pos + 1, filename.size() - (pos + 1) );
+}
+
+
+/// returns the extension(suffix) of the filename
+inline std::string get_nopathfilename( const std::string& filename )
+{
+	size_t pos = filename.rfind("\\");
+
+	if( pos != std::string::npos
+	 && pos != filename.length() - 1 ) 
+		return filename.substr( pos+1, filename.length() - (pos+1) );
+	else
+	{
+		pos = filename.rfind("/");
+		if( pos != std::string::npos
+		 && pos != filename.length() - 1 ) 
+			return filename.substr( pos+1, filename.length() - (pos+1) );
+	}
+
+	return std::string("");
+}
+
+
+/// returns the current working directory (absolute path to the current directory)
+inline std::string get_cwd()
+{
+	char strCWD[LFS_MAX_PATH];
+//	GetCurrentDirectory( LFS_MAX_PATH, strCWD );
+	getcwd( strCWD, LFS_MAX_PATH );
+	return std::string(strCWD);
+}
+
+
+/// set the working directory
+inline bool set_wd( const std::string& workdir )
+{
+//	SetCurrentDirectory( workdir.c_str() );
+	if( _chdir( workdir.c_str() ) == 0 )
+		return true;
+	else
+		return false;
+}
+
+
+/// Return the directory path of the given path
+/// Returns an empty string if 'filepath' does not include directory path.
+/// Returned directory path does not contain the separator charactor ( '/' or '\\' )
+/// at the end.
+inline std::string get_dirpath( const std::string& filepath )
+{
+	struct stat st;
+	if( stat( filepath.c_str(), &st ) == 0 )
+	{
+		std::string sep_str[2] = { "\\", "/" };
+		if( st.st_mode == S_IFDIR )
+		{
+			// a directory path
+			for( int i=0; i<2; i++ )
+			{
+				// remove the trailing separator character
+				if( filepath.substr( filepath.length()-1, 1 ) == sep_str[i] )
+					return filepath.substr( 0, filepath.length()-1 );
+			}
+
+			return filepath;
+		}
+
+		// extract directory path from the filepath
+
+		size_t last_sep_pos = 0;
+		for( int i=0; i<2; i++ )
+		{
+			size_t pos = filepath.rfind( sep_str[i] );
+			if( pos != std::string::npos
+			 && last_sep_pos < pos )
+			{
+				last_sep_pos = pos;
+			}
+		}
+
+		if( last_sep_pos == 0 )
+			return std::string(""); // 'filepath' has no directory path - return an empty string
+		else
+			return filepath.substr( 0, last_sep_pos );
+	}
+
+	return std::string("");		 
+}
+
+
+inline bool is_abspath( const std::string& filename )
+{
+	if( filename.find( ":\\" ) == 1 || filename.find( ":/" ) == 1 )
+		return true;
+	else
+		return false;
+}
+
+
+/// returns a filename without path
+inline std::string get_path_after_dirpath( const std::string& filename )
+{
+	size_t pos = filename.rfind( "/" );
+	if( pos == std::string::npos )
+	{
+		pos = filename.rfind( "\\" );
+	}
+
+	if( pos == std::string::npos )
+	{
+		return filename;
+	}
+
+	return filename.substr( pos+1, filename.length() - (pos+1) );
+}
+
+
+/// returns a filename without suffix
+inline std::string get_no_ext( const std::string& filename )
+{
+	size_t pos = filename.rfind( "." );
+	if( pos == std::string::npos )
+		return filename;
+
+	return filename.substr( 0, pos );
+}
+
+/**
+ insert a string before the suffix
+ - example: string filename = "readme.txt"; append_before_ext( filename, "_en" );
+ - result: filename = "readme_en.txt"
+*/
+inline void append_before_ext( std::string& dest_filename, const std::string& str )
+{
+	size_t dot_pos = dest_filename.rfind( "." );
+	if( dot_pos == std::string::npos )
+	{
+		dest_filename += str;
+	}
+
+	dest_filename.insert( dot_pos, str );
+}
+
+
+
+inline int get_filesize( const std::string& filename )
+{
+	struct stat results;
+
+	if( stat(filename.c_str(), &results) == 0 )
+		return (int)(results.st_size);
+	else
+		return -1;
+}
+
+
+/// returns true if the file exists
+inline bool file_exists( const std::string& filename )
+{
+	FILE* fp = fopen( filename.c_str(), "r" );
+	if( fp )
+	{
+		fclose(fp);
+		return true;
+	}
+	else
+		return false;
+}
+
+
+/// returns true if the path exists
+inline bool path_exists( const std::string& pathname )
+{
+	struct stat results;
+
+	int ret = stat(pathname.c_str(), &results);
+
+	if( ret == 0 )
+		return true;
+	else if( ret == -1 )
+		return false;
+	else if( ret == EINVAL )
+	{
+		assert( !"path_exists() - EINVAL returned from stat()" );
+		return false;
+	}
+	else
+		return false; // some unknown error
+}
+
+
+/// returns the last modified time of the file
+inline time_t get_last_modified_time( const std::string& filename )
+{
+	struct stat obj_info;
+	int ret = stat( filename.c_str(), &obj_info );
+
+	if( ret == 0 )
+		return obj_info.st_mtime;
+	else
+		return 0;	// error
+}
+
+
+inline void get_filedata( const std::string& filename, std::vector<unsigned char>& rDestBuffer )
+{
+	FILE *fp = fopen( filename.c_str(), "rb" );
+	if( !fp )
+		return;
+
+	int filesize = get_filesize(filename);
+	if( filesize < 0 )
+		return;
+
+	rDestBuffer.resize(filesize);
+	fread( &rDestBuffer[0], 1, filesize, fp );
+
+	fclose(fp);
+}
+
+
+class dir_stack
+{
+	enum params { PATH_BUFFER_SIZE = 512 };
+
+	std::vector<std::string> m_vecDirectory;
+
+	char m_buffer[PATH_BUFFER_SIZE];
+
+public:
+
+	dir_stack() {}
+
+	dir_stack( const std::string& path )
+	{
+		push_cwd_and_chdir( path );
+	}
+	
+	~dir_stack()
+	{
+	}
+
+	/// Push the current working directory to the stack and
+	/// set the working directory specified by the argument 'path'
+	bool push_cwd_and_chdir( const std::string& path )
+//	bool setdir( const std::string& path )
+	{
+		if( getcwd( m_buffer, PATH_BUFFER_SIZE-1 ) )
+		{
+			// save the current directory
+			m_vecDirectory.push_back( std::string( m_buffer ) );
+
+			// set the new directory
+			if( _chdir( path.c_str() ) == 0 )
+			{
+				return true;
+			}
+			else
+			{
+				// the specified path was not found
+				m_vecDirectory.pop_back();
+				return false;
+			}
+		}
+		else
+			return false;
+	}
+
+	/// Pop the directory path from the stack and change
+	/// the working directory to it.
+	bool pop_and_chdir()
+//	bool prevdir()
+	{
+		if( m_vecDirectory.size() == 0 )
+			return false;	// no directory is in the stack
+
+		const std::string prev_path = m_vecDirectory.back();
+		if( _chdir( prev_path.c_str() ) == 0 )
+		{
+			m_vecDirectory.pop_back();	// delete the last path at the top of the stack
+			return true;
+		}
+		else
+			return false;
+	}
+};
+
+
+} // namespace lfs
+
+
+#endif  /*  __LFS_HPP__  */
