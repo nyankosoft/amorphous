c7d87cf896d09e036a8052aa32211603155cb021
blue <blue@1b90e699-09eb-4805-8378-d520347fdb25>
2009-09-20 02:07:08 +0000

    added: source and project files of PostProcessEffectTest

---
+++ Graphics/PostProcessEffectTest/src/PostProcessEffectTest.cpp
@@ -0,0 +1,581 @@
+#include "PostProcessEffectTest.hpp"
+#include "Graphics/D3DXMeshObjectBase.hpp"
+#include "Graphics/Shader/D3DFixedFunctionPipelineManager.hpp"
+#include "Graphics/MeshGenerators.hpp"
+#include "Support/CameraController_Win32.hpp"
+
+
+using namespace std;
+using namespace boost;
+
+
+extern CPlatformDependentCameraController g_CameraController;
+
+
+const std::string GetAppTitle()
+{
+	return string("PostProcessEffectTest");
+}
+
+
+CGraphicsTestBase *CreateTestInstance()
+{
+	return new CPostProcessEffectTest();
+}
+
+/*
+void CPostProcessEffectTestInputHandler::ProcessInput( SInputData& input )
+{
+	m_pTest->ProcessInput( input );
+}
+*/
+
+void CPostProcessEffectTest::HandleInput( const SInputData& input )
+{
+	switch( input.iGICode )
+	{
+	case '1':
+//	case 'R':
+//		m_pSynthTest->m_pSynthesizer->SetRootPose( Matrix34Identity() );
+		break;
+
+	case 'H':
+		if( input.iType == ITYPE_KEY_PRESSED )
+		{
+			if( !m_pPostProcessEffectManager )
+				return;
+
+			if( !m_pPostProcessEffectManager->IsEnabled(CPostProcessEffect::TF_HDR_LIGHTING) )
+				m_pPostProcessEffectManager->EnableHDRLighting( true );
+			else
+				m_pPostProcessEffectManager->EnableHDRLighting( false );
+		}
+		break;
+
+	case 'B':
+		if( input.iType == ITYPE_KEY_PRESSED )
+		{
+			if( !m_pPostProcessEffectManager )
+				return;
+
+			if( !m_pPostProcessEffectManager->IsEnabled(CPostProcessEffect::TF_BLUR) )
+				m_pPostProcessEffectManager->EnableBlur( true );
+			else
+				m_pPostProcessEffectManager->EnableBlur( false );
+
+			// set the blur effect
+		}
+		break;
+
+	case 'N':
+/*		if( input.iType == ITYPE_KEY_PRESSED )
+		{
+			// set the negative effect
+			m_aPostProcessEffect[PP_NEGATIVE] = 1;
+			m_pPPManager->GetPostProcessInstance().push_back( CPProcInstance(PP_COLOR_INVERSE) );
+		}*/
+		break;
+
+	case 'V':
+/*		if( input.iType == ITYPE_KEY_PRESSED )
+		{
+			// set the blur effect
+			m_aPostProcessEffect[PP_GBLUR2] = 1;
+			m_aFilterIndex[SF_DOWNFILTER_FOR_BLUR] = m_pPPManager->GetPostProcessInstance().size();
+			m_pPPManager->GetPostProcessInstance().push_back( CPProcInstance( PP_COLOR_DOWNFILTER4 ) );
+			m_pPPManager->GetPostProcessInstance().push_back( CPProcInstance( PP_COLOR_GBLUR_H ) );
+			m_pPPManager->GetPostProcessInstance().push_back( CPProcInstance( PP_COLOR_GBLUR_V ) );
+			m_aFilterIndex[SF_UPFILTER_FOR_BLUR] = m_pPPManager->GetPostProcessInstance().size();
+			m_pPPManager->GetPostProcessInstance().push_back( CPProcInstance( PP_COLOR_UPFILTER4 ) );
+		}*/
+		break;
+
+	case 'K':
+/*		if( input.iType == ITYPE_KEY_PRESSED )
+		{
+			m_aPostProcessEffect[PP_COARSE] = 1;
+			m_pPPManager->GetPostProcessInstance().push_back( CPProcInstance( PP_COLOR_DOWNFILTER4 ) );
+			m_pPPManager->GetPostProcessInstance().push_back( CPProcInstance( PP_COLOR_UPFILTER4 ) );
+		}*/
+		break;
+
+	case 'L':
+/*		if( input.iType == ITYPE_KEY_PRESSED )
+		{
+			m_aPostProcessEffect[PP_BLOOM] = 1;
+			m_aFilterIndex[SF_BRIGHTPASS] = m_pPPManager->GetPostProcessInstance().size();
+			m_pPPManager->GetPostProcessInstance().push_back( CPProcInstance( PP_COLOR_BRIGHTPASS ) );
+		}*/
+		break;
+
+	case 'G':
+/*		if( input.iType == ITYPE_KEY_PRESSED )
+		{
+			// set the bloom effect
+			m_aPostProcessEffect[PP_BLOOM] = 1;
+			m_pPPManager->GetPostProcessInstance().push_back( CPProcInstance( PP_COLOR_DOWNFILTER4 ) );
+			m_pPPManager->GetPostProcessInstance().back().SetScale( 0.25f, 0.25f );
+			m_pPPManager->GetPostProcessInstance().push_back( CPProcInstance( PP_COLOR_DOWNFILTER4 ) );
+			m_pPPManager->GetPostProcessInstance().back().SetScale( 0.25f, 0.25f );
+
+			m_aFilterIndex[SF_BRIGHTPASS] = m_pPPManager->GetPostProcessInstance().size();
+			m_pPPManager->GetPostProcessInstance().push_back( CPProcInstance( PP_COLOR_BRIGHTPASS ) );
+
+			// set luminance. (smaller luminance value causes more glare)
+			m_pPPManager->GetPostProcessInstance().back().m_avParam[0].x = 0.08f;
+
+			m_pPPManager->GetPostProcessInstance().push_back( CPProcInstance( PP_COLOR_BLOOM_H ) );
+			m_pPPManager->GetPostProcessInstance().back().m_avParam[0].x = 1.5f;
+			m_pPPManager->GetPostProcessInstance().push_back( CPProcInstance( PP_COLOR_BLOOM_V ) );
+			m_pPPManager->GetPostProcessInstance().back().m_avParam[0].x = 1.5f;
+			m_pPPManager->GetPostProcessInstance().push_back( CPProcInstance( PP_COLOR_BLOOM_H ) );
+			m_pPPManager->GetPostProcessInstance().back().m_avParam[0].x = 1.5f;
+			m_pPPManager->GetPostProcessInstance().push_back( CPProcInstance( PP_COLOR_BLOOM_V ) );
+			m_pPPManager->GetPostProcessInstance().back().m_avParam[0].x = 1.5f;
+
+			m_pPPManager->GetPostProcessInstance().push_back( CPProcInstance( PP_COLOR_UPFILTER4 ) );
+			m_pPPManager->GetPostProcessInstance().back().SetScale( 4.0f, 4.0f );
+			m_pPPManager->GetPostProcessInstance().push_back( CPProcInstance( PP_COLOR_COMBINE4 ) );
+			m_pPPManager->GetPostProcessInstance().back().SetScale( 4.0f, 4.0f );
+		}*/
+		break;
+
+	case 'C':
+/*		if( input.iType == ITYPE_KEY_PRESSED )
+		{
+			m_pPPManager->GetPostProcessInstance().resize(0);
+			for( int i=0; i<NUM_PP_EFFECTS; i++ )
+				m_aPostProcessEffect[i] = 0;
+		}*/
+		break;
+/*
+	case VK_MULTIPLY:
+		if( input.iType == ITYPE_KEY_PRESSED )
+		{
+			m_fBlurFactor += 0.2f;
+			if( 2.0f < m_fBlurFactor )
+				m_fBlurFactor = 2.0f;
+		}
+		break;
+
+	case VK_DIVIDE:
+		if( input.iType == ITYPE_KEY_PRESSED )
+		{
+			m_fBlurFactor -= 0.2f;
+			if( m_fBlurFactor < 0.01f )
+				m_fBlurFactor = 0.01f;
+		}
+		break;
+
+	case VK_PRIOR:
+		if( input.iType == ITYPE_KEY_PRESSED )
+		{
+			if( m_aPostProcessEffect[PP_BLOOM] )
+			{
+				float& luminance = m_pPPManager->GetPostProcessInstance()[m_aFilterIndex[SF_BRIGHTPASS]].m_avParam[0].x;
+				luminance += 0.01f;
+				if( 2.0f < luminance )
+					luminance = 2.0f;
+			}
+		}
+		break;
+
+	case VK_NEXT:
+		if( input.iType == ITYPE_KEY_PRESSED )
+		{
+			if( m_aPostProcessEffect[PP_BLOOM] )
+			{
+				float& luminance = m_pPPManager->GetPostProcessInstance()[m_aFilterIndex[SF_BRIGHTPASS]].m_avParam[0].x;
+				luminance -= 0.01f;
+				if( luminance < 0.01f )
+					luminance = 0.01f;
+			}
+		}
+		break;
+*/
+	default:
+		break;
+	}
+}
+
+
+void RenderAsSkybox( CMeshObjectHandle& mesh )
+{
+	LPDIRECT3DDEVICE9 pd3dDevice = DIRECT3D9.GetDevice();
+
+	pd3dDevice->SetRenderState( D3DRS_ALPHABLENDENABLE, FALSE );
+	pd3dDevice->SetRenderState( D3DRS_ALPHATESTENABLE,  FALSE );
+	pd3dDevice->SetRenderState( D3DRS_LIGHTING, FALSE );
+	pd3dDevice->SetRenderState( D3DRS_ZENABLE, D3DZB_FALSE );
+	pd3dDevice->SetRenderState( D3DRS_ZWRITEENABLE, FALSE );
+
+	shared_ptr<CD3DXMeshObjectBase> pMesh = mesh.GetMesh();
+
+	CD3DFixedFunctionPipelineManager ffp_mgr;
+
+	D3DXMATRIX matWorld;
+	D3DXMatrixIdentity( &matWorld );
+	const Vector3 vCamPos = g_CameraController.GetPosition();
+	matWorld._41 = vCamPos.x;
+	matWorld._42 = vCamPos.y;
+	matWorld._43 = vCamPos.z;
+	ffp_mgr.SetWorldTransform( matWorld );
+
+	if( pMesh )
+		pMesh->Render( ffp_mgr );
+
+	pd3dDevice->SetRenderState( D3DRS_ZENABLE, D3DZB_TRUE );
+	pd3dDevice->SetRenderState( D3DRS_ZWRITEENABLE, TRUE );
+}
+
+
+
+CPostProcessEffectTest::CPostProcessEffectTest()
+:
+m_fBlurFactor(2.0f),
+m_fLuminance(0.08f),
+m_PPEffectFlags(0)
+{
+}
+
+
+CPostProcessEffectTest::~CPostProcessEffectTest()
+{
+}
+
+
+void CPostProcessEffectTest::RenderMeshes()
+{
+	HRESULT hr;
+	LPDIRECT3DDEVICE9 pd3dDevice = DIRECT3D9.GetDevice();
+
+	// alpha-blending settings 
+//	pd3dDevice->SetRenderState( D3DRS_ALPHABLENDENABLE, TRUE );
+	pd3dDevice->SetRenderState( D3DRS_ALPHABLENDENABLE, FALSE );
+//	pd3dDevice->SetRenderState( D3DRS_SRCBLEND, D3DBLEND_SRCALPHA );
+//	pd3dDevice->SetRenderState( D3DRS_DESTBLEND, D3DBLEND_INVSRCALPHA );
+
+	pd3dDevice->SetRenderState( D3DRS_ALPHATESTENABLE, TRUE );
+	pd3dDevice->SetRenderState( D3DRS_ALPHAREF,  (DWORD)0x00000001 );
+	pd3dDevice->SetRenderState( D3DRS_ALPHAFUNC, D3DCMP_GREATEREQUAL );	// draw a pixel if its alpha value is greater than or equal to '0x00000001'
+
+	CShaderManager *pShaderManager = m_Shader.GetShaderManager();
+	LPD3DXEFFECT pEffect = pShaderManager->GetEffect();
+	if( pEffect )
+	{
+		hr = pEffect->SetValue( "m_vEyePos", &(g_Camera.GetPosition()), sizeof(float) * 3 );
+
+//		hr = pEffect->SetTechnique( "Default" );
+//		hr = pEffect->SetTechnique( "NullShader" );
+		hr = pEffect->SetTechnique( "QuickTest" );
+	}
+
+//	if( FAILED(hr) )
+//		return;
+
+	pd3dDevice->SetVertexShader( NULL );
+	pd3dDevice->SetPixelShader( NULL );
+
+	size_t i, num_meshes = m_vecMesh.size();
+
+	if( 0 < num_meshes )
+	{
+		RenderAsSkybox( m_vecMesh[0] );
+	}
+
+	// reset the world transform matrix
+	CD3DFixedFunctionPipelineManager ffp_mgr;
+	D3DXMATRIX matWorld;
+	D3DXMatrixIdentity( &matWorld );
+	ffp_mgr.SetWorldTransform( matWorld );
+
+	for( i=1; i<num_meshes; i++ )
+	{
+		shared_ptr<CD3DXMeshObjectBase> pMesh = m_vecMesh[i].GetMesh();
+
+		if( pMesh )
+			pMesh->Render( ffp_mgr );
+	}
+
+/*
+	UINT cPasses;
+	pEffect->Begin( &cPasses, 0 );
+
+	size_t i, num_meshes = m_vecMesh.size();
+	for( i=0; i<num_meshes; i++ )
+	{
+		CD3DXMeshObject *pMeshObject = m_vecMesh[i];
+
+		if( !pMeshObject )
+			continue;
+
+		LPD3DXBASEMESH pMesh = pMeshObject->GetBaseMesh();
+		if( !pMesh )
+			return;
+
+		pd3dDevice->SetVertexDeclaration( pMeshObject->GetVertexDeclaration() );
+
+		LPDIRECT3DTEXTURE9 pTex;
+
+		// Meshes are divided into subsets by materials. Render each subset in a loop
+		DWORD mat, num_materials = pMeshObject->GetNumMaterials();
+		for( mat=0; mat<num_materials; mat++ )
+		{
+			pTex = pMeshObject->GetTexture(mat);
+			if( pTex )
+                m_pShaderManager->SetTexture( 0, pTex );
+			else
+				m_pShaderManager->SetTexture( 0, m_pDefaultTexture );
+
+			pEffect->CommitChanges();
+
+			for( UINT p = 0; p < cPasses; ++p )
+			{
+				pEffect->BeginPass( p );
+
+				// Draw the mesh subset
+				pMesh->DrawSubset( mat );
+
+				pEffect->EndPass();
+			}
+		}
+	}
+
+	pEffect->End();
+*/
+}
+
+
+void CPostProcessEffectTest::UpdateShaderParams()
+{
+/*	float weight[7];
+
+	float x2;
+	float s2 = m_fBlurFactor * m_fBlurFactor;
+	for( int i=0; i<7; i++ )
+	{
+		x2 = (float)(i*i);
+		weight[i] = pow( 2.72f, - 0.5f * x2 / s2 ); 
+//		weight[i] = 0.2f * pow( 2.72f, - 0.5f * x2 / s2 ); 
+	}
+
+	float sum = weight[0];
+	for( int i=1; i<7; i++ )
+		sum += weight[i] * 2.0f;
+
+	for( int i=0; i<7; i++ )
+		weight[i] /= sum;
+
+	LPD3DXEFFECT pEffect[2];
+	pEffect[0] = m_pPPManager->GetPostProcess(m_aPPEffectIndex[PP_COLOR_GBLUR_H]).GetEffect();
+	pEffect[1] = m_pPPManager->GetPostProcess(m_aPPEffectIndex[PP_COLOR_GBLUR_V]).GetEffect();
+
+	HRESULT hr;
+	char str[32];
+	for( int j=0; j<2; j++ )
+	{
+		if( !pEffect[j] )
+			continue;
+
+		pEffect[j]->SetFloat( "BlurWeights[6]", weight[0] );
+
+		for( int i=1; i<6; i++ )
+		{
+			sprintf( str, "BlurWeights[%d]", 6-i );
+			hr = pEffect[j]->SetFloat( str, weight[i] );
+
+			sprintf( str, "BlurWeights[%d]", 6+i );
+			hr = pEffect[j]->SetFloat( str, weight[i] );
+		}
+	}
+
+//	float up_scale = m_fBlurFactor * 2.0f;
+//	float down_scale = 1.0f / up_scale;
+
+	float up_scale = ( m_fBlurFactor + 1.0f ) * 2.0f;
+	float down_scale = 1.0f / up_scale;
+
+//	m_pPPManager->GetPostProcess(m_aPPEffectIndex[PP_COLOR_DOWNFILTER4]).SetScale( down_scale, down_scale );
+//	m_pPPManager->GetPostProcess(m_aPPEffectIndex[PP_COLOR_UPFILTER4]).SetScale( up_scale, up_scale );
+
+	if( m_aPostProcessEffect[PP_GBLUR2] )
+	{
+		m_pPPManager->GetPostProcessInstance()[m_aFilterIndex[SF_DOWNFILTER_FOR_BLUR]].SetScale( down_scale, down_scale );
+		m_pPPManager->GetPostProcessInstance()[m_aFilterIndex[SF_UPFILTER_FOR_BLUR]].SetScale( up_scale, up_scale );
+	}*/
+}
+
+
+void CPostProcessEffectTest::RenderScene()
+{
+//	RenderSkybox();
+
+	RenderMeshes();
+}
+
+
+//-----------------------------------------------------------------------------
+// Name: Render()
+// Desc: Draws the scene
+//-----------------------------------------------------------------------------
+void CPostProcessEffectTest::Render()
+{
+//	DIRECT3D9.GetDevice()->BeginScene();
+	UpdateShaderParams();
+//	DIRECT3D9.GetDevice()->EndScene();
+
+	LPDIRECT3DDEVICE9 pd3dDevice = DIRECT3D9.GetDevice();
+//	HRESULT hr;
+
+	// update the camera matrix
+	//	m_CameraController.SetCameraMatrix();
+	//	D3DXMATRIX matCamera;
+	//	m_CameraController.GetCameraMatrix( matCamera );
+	//	m_pShaderManager->SetViewTransform( matCamera );
+
+	// clear the backbuffer to a blue color
+	//	pd3dDevice->Clear( 0, NULL, D3DCLEAR_TARGET|D3DCLEAR_ZBUFFER, D3DCOLOR_XRGB(0,0,255), 1.0f, 0 );
+
+	if( m_pPostProcessEffectManager )
+		m_pPostProcessEffectManager->BeginRender();
+
+	RenderScene();
+
+
+	if( m_pPostProcessEffectManager )
+	{
+		m_pPostProcessEffectManager->EndRender();
+		m_pPostProcessEffectManager->RenderPostProcessEffects();
+	}
+
+//	D3DXMatrixIdentity( &matCamera );
+//	m_CameraController.GetCameraMatrix( matCamera );
+//	m_pShaderManager->SetViewTransform( matCamera );
+
+/*
+	// render the text info
+	char acStr[256];
+	sprintf( acStr, "%f", TIMER.GetFPS() );
+	m_Font.DrawText( acStr, D3DXVECTOR2(20,20), 0xFFFFFFFF );
+*/
+	// present the backbuffer contents to the display
+	//	pd3dDevice->Present( NULL, NULL, NULL, NULL );
+}
+
+
+bool CPostProcessEffectTest::LoadModels()
+{
+	m_vecMeshFilepath.resize( 2 );
+	m_vecMeshFilepath[0] = ""; // skybox
+	m_vecMeshFilepath[1] = "./models/compact_000-r01-low_poly.msh";
+
+	m_vecMesh.resize( 2 );
+	CMeshResourceDesc skybox_mesh_desc;
+	shared_ptr<CBoxMeshGenerator> pBoxMeshGenerator = shared_ptr<CBoxMeshGenerator>( new CBoxMeshGenerator );
+	pBoxMeshGenerator->SetPolygonDirection( MeshPolygonDirection::INWARD );
+	pBoxMeshGenerator->SetEdgeLengths( Vector3(1,1,1) );
+	pBoxMeshGenerator->SetTexCoordStyleFlags( TexCoordStyle::LINEAR_SHIFT_INV_Y );
+	pBoxMeshGenerator->SetPolygonDirection( MeshPolygonDirection::INWARD );
+	pBoxMeshGenerator->SetTexturePath( "models/textures/skygrad_slim_01.jpg" );
+	skybox_mesh_desc.pMeshGenerator = pBoxMeshGenerator;
+	bool loaded = m_vecMesh[0].Load( skybox_mesh_desc );
+
+	loaded = m_vecMesh[1].Load( m_vecMeshFilepath[1] );
+
+	return true;
+}
+
+
+void SetDefaultLinearFog()
+{
+	LPDIRECT3DDEVICE9 pd3dDev = DIRECT3D9.GetDevice();
+
+	float fStart   = 0.50f;    // For linear mode
+	float fEnd     = 480.0f;//0.95f;
+	float fDensity = 0.66f;   // For exponential modes
+
+	DWORD fog_color = 0xFFDDDFDE;
+
+	HRESULT hr;
+	hr = pd3dDev->SetRenderState( D3DRS_FOGENABLE, TRUE );
+	hr = pd3dDev->SetRenderState( D3DRS_FOGTABLEMODE, D3DFOG_LINEAR );
+	hr = pd3dDev->SetRenderState( D3DRS_FOGCOLOR, fog_color);
+	hr = pd3dDev->SetRenderState( D3DRS_FOGSTART, *(DWORD *)(&fStart));
+	hr = pd3dDev->SetRenderState( D3DRS_FOGEND,   *(DWORD *)(&fEnd));
+
+//	hr = pd3dDev->SetRenderState( D3DRS_FOGTABLEMODE, D3DFOG_EXP);
+//	hr = pd3dDev->SetRenderState(D3DRS_FOGDENSITY, *(DWORD *)(&fDensity));
+}
+
+
+int CPostProcessEffectTest::Init()
+{
+//	m_pShaderManager
+//		= shared_ptr<CShaderManager>( new CShaderManager() );
+
+	g_CameraController.SetPosition( Vector3(0,50,0) );
+
+	bool loaded = m_Shader.Load( "shaders/mesh.fx" );
+
+//	if( !m_pShaderManager->LoadShaderFromFile( "shaders/mesh.fx" ) )
+	if( !loaded )
+		return 1;
+
+	CShaderManager *pShaderManager = m_Shader.GetShaderManager();
+
+//	CShader::Get()->SetShaderManager( &m_pShaderManager );
+
+	// set the world matrix to the identity
+	D3DXMATRIX matWorld;
+	D3DXMatrixIdentity( &matWorld );
+	DIRECT3D9.GetDevice()->SetTransform( D3DTS_WORLD, &matWorld );
+	pShaderManager->SetWorldTransform( matWorld );
+
+	// set the projection matrix
+    D3DXMATRIX matProj;
+    D3DXMatrixPerspectiveFovLH( &matProj, D3DX_PI / 4, 640.0f / 480.0f, 0.5f, 320.0f );
+    DIRECT3D9.GetDevice()->SetTransform( D3DTS_PROJECTION, &matProj );
+	pShaderManager->SetProjectionTransform( matProj );
+/*
+	// initialize the light for the shader
+	m_ShaderLightManager.Init();
+
+	// create a light for the scene
+	CHemisphericDirLight light;
+	light.UpperColor.SetRGBA( 1.00f, 1.00f, 1.00f, 1.00f );
+	light.LowerColor.SetRGBA( 0.25f, 0.25f, 0.25f, 1.00f );
+	Vector3 vDir = Vector3(-1.0f,-3.0f,-1.5f);
+	Vec3Normalize( vDir, vDir );
+	light.vDirection = vDir;
+	m_ShaderLightManager.SetLight( 0, light );
+*/
+	// create default texture
+//	DWORD dwColor = 0xFFF0F0F0;
+//	CTextureTool::CreateTexture( &dwColor, 1, 1, &m_pDefaultTexture );
+
+	D3DSURFACE_DESC back_buffer_desc;
+	IDirect3DSurface9 *pBackBuffer;
+	DIRECT3D9.GetDevice()->GetBackBuffer( 0, 0, D3DBACKBUFFER_TYPE_MONO, &pBackBuffer );
+	pBackBuffer->GetDesc( &back_buffer_desc );
+
+	LPDIRECT3DDEVICE9 pd3dDev = DIRECT3D9.GetDevice();
+//	HRESULT hr;
+
+	SetDefaultLinearFog();
+
+	// init post process effect manager
+	bool test_ppeffect_mgr = true;
+	if( test_ppeffect_mgr )
+	{
+		m_pPostProcessEffectManager = shared_ptr<CPostProcessEffectManager>( new CPostProcessEffectManager );
+
+		Result::Name res = m_pPostProcessEffectManager->Init( "shaders" );
+		if( res != Result::SUCCESS )
+			return -1;
+	}
+
+	// load models
+	loaded = LoadModels();
+
+	return 0;
+}
---
+++ Graphics/PostProcessEffectTest/src/PostProcessEffectTest.hpp
@@ -0,0 +1,158 @@
+#ifndef  __PostProcessEffectTest_HPP__
+#define  __PostProcessEffectTest_HPP__
+
+
+#include <vector>
+#include <string>
+#include <boost/shared_ptr.hpp>
+using namespace boost;
+
+#include "3DMath/Vector3.hpp"
+#include "Graphics/fwd.hpp"
+#include "Graphics/Camera.hpp"
+#include "Graphics/MeshObjectHandle.hpp"
+#include "Graphics/TextureHandle.hpp"
+#include "Graphics/ShaderHandle.hpp"
+#include "Graphics/Shader/ShaderTechniqueHandle.hpp"
+#include "Graphics/Shader/ShaderManager.hpp"
+#include "Graphics/Shader/ShaderLightManager.hpp"
+#include "Graphics/PostProcessEffectManager.hpp"
+#include "Input/fwd.hpp"
+#include "Input.hpp"
+#include "Graphics/UnitSphere.hpp"
+
+#include "../../../_Common/GraphicsTestBase.hpp"
+
+
+enum ePPEffectFile
+{
+	PP_COLOR_GBLUR_H,
+	PP_COLOR_GBLUR_V,
+    PP_COLOR_INVERSE,
+	PP_COLOR_MONOCHROME,
+	PP_COLOR_DOWNFILTER4,
+	PP_COLOR_UPFILTER4,
+	PP_COLOR_BLOOM_H,
+	PP_COLOR_BLOOM_V,
+	PP_COLOR_BRIGHTPASS,
+	PP_COLOR_COMBINE4,
+	NUM_PP_EFFECT_FILES
+};
+
+enum ePPEffect
+{
+	PP_GBLUR,
+	PP_GBLUR2,
+	PP_COARSE,
+	PP_NEGATIVE,
+	PP_BLOOM,
+	NUM_PP_EFFECTS
+};
+
+enum eSizeFilterType
+{
+	SF_DOWNFILTER_FOR_BLUR,
+	SF_UPFILTER_FOR_BLUR,
+	SF_DOWNFILTER_FOR_BLOOM,
+	SF_UPFILTER_FOR_BLOOM,
+	SF_BRIGHTPASS,
+	NUM_SIZE_FILTERS,
+};
+
+/*
+class CPostProcessEffectTest;
+
+
+class CPostProcessEffectTestInputHandler : public CInputHandler
+{
+	CPostProcessEffectTest* m_pTest;
+
+public:
+
+	CPostProcessEffectTestInputHandler( CPostProcessEffectTest* pTest )
+		:
+	m_pTest(pTest) {}
+
+	virtual void ProcessInput(SInputData& input);
+};
+*/
+
+class CPostProcessEffectTest : public CGraphicsTestBase
+{
+//	shared_ptr<CPostProcessManager> m_pPPManager;
+	boost::shared_ptr<CPostProcessEffectManager> m_pPostProcessEffectManager;
+
+	int m_aPPEffectIndex[NUM_PP_EFFECT_FILES];
+
+	int m_aPostProcessEffect[NUM_PP_EFFECTS];
+
+	float m_fBlurFactor;
+
+	int m_aFilterIndex[NUM_SIZE_FILTERS];
+
+	float m_fLuminance;
+
+	U32 m_PPEffectFlags;
+
+	std::vector<CMeshObjectHandle> m_vecMesh;
+
+	std::vector<std::string> m_vecMeshFilepath;
+
+	CShaderTechniqueHandle m_MeshTechnique;
+	CShaderTechniqueHandle m_SkyboxTechnique;
+	CShaderTechniqueHandle m_DefaultTechnique;
+
+	CShaderHandle m_Shader;
+
+//	shared_ptr<CShaderManager> m_pShaderManager;
+
+	CShaderLightManager m_ShaderLightManager;
+
+	CTextureHandle m_TestTexture;
+	CTextureHandle m_DefaultTexture;
+
+	CUnitSphere m_LightPosIndicator;
+
+//	CShaderLightManager m_ShaderLightManager;
+
+	Vector3 m_vLightPosition;
+
+	CFontSharedPtr m_pFont;
+
+	CInputHandlerSharedPtr m_pInputHandler;
+
+protected:
+
+	void InitLensFlare( const std::string& strPath );
+
+	void RenderMeshes();
+
+	void UpdateShaderParams();
+
+	bool LoadModels();
+
+public:
+
+	CPostProcessEffectTest();
+
+	~CPostProcessEffectTest();
+
+	int Init();
+
+	void Release() {};
+
+//	void Update( float dt );
+
+	void Render();
+
+	void RenderScene();
+
+//	void UpdateViewTransform( const D3DXMATRIX& matView );
+
+//	void UpdateProjectionTransform( const D3DXMATRIX& matProj );
+
+	void HandleInput( const SInputData& input );
+};
+
+
+#endif /* __PostProcessEffectTest_HPP__ */
