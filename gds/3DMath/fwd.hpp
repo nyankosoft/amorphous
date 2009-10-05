39bfcf1c90690f6b245f5062466d2dec29dab039
blue <blue@b80c8c75-5753-0c4f-8f2c-6ef837923b1e>
2009-10-05 16:52:51 +0000

    added: 3DMath/fwd.hpp

---
+++ gds/3DMath/fwd.hpp
@@ -0,0 +1,19 @@
+#ifndef __FWD_3DMath_HPP__
+#define __FWD_3DMath_HPP__
+
+class Vector2;
+class Matrix22;
+class Matrix23;
+class AABB2;
+
+class Vector3;
+class Matrix33;
+class Matrix34;
+class AABB3;
+class Quaternion;
+class CTriangle;
+class Sphere;
+struct SPlane;
+
+
+#endif /* __FWD_3DMath_HPP__ */
