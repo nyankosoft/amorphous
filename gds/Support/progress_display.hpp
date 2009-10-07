aaf7e524c2b293a1cb5c2815df2272cd6a25010a
blue <blue@b80c8c75-5753-0c4f-8f2c-6ef837923b1e>
2009-10-07 14:45:58 +0000

    added: Support/progress_display.hpp   - Holds progress of a task.

---
+++ gds/Support/progress_display.hpp
@@ -0,0 +1,49 @@
+#ifndef  __progress_display_HPP__
+#define  __progress_display_HPP__
+
+
+namespace morph
+{
+
+
+// Does not do auto-scaling
+
+class progress_display
+{
+	int m_NumCurrentUnits;
+	int m_NumTotalUnits;
+
+public:
+
+	progress_display( int num_total_units = 1 )
+		:
+	m_NumCurrentUnits(0),
+	m_NumTotalUnits(num_total_units)
+	{
+	}
+
+	~progress_display() {};
+
+	void operator+=( int num_units )
+	{
+		m_NumCurrentUnits += num_units;
+	}
+
+	void operator++()
+	{
+		(*this) += 1;
+	}
+
+	int get_current_units() const { return m_NumCurrentUnits; }
+	int get_total_units() const { return m_NumTotalUnits; }
+
+	void set_current_units( int num_units ) { m_NumCurrentUnits = num_units; }
+	void set_total_units( int num_total_units ) { m_NumTotalUnits = num_total_units; }
+};
+
+
+} // morph
+
+
+
+#endif		/*  __progress_display_HPP__  */
