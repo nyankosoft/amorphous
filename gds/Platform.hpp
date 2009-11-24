414739e3b3babd68da2f8b6301c15733c8b23ee4
blue <blue@b80c8c75-5753-0c4f-8f2c-6ef837923b1e>
2009-11-24 14:09:09 +0000

    changed: split the header that defines platform-specific macros to hide platform-dependent code from client.   - Client code includes "Platform.hpp" and does not know which platform program is intended to run on.

---
+++ gds/Platform.hpp
@@ -0,0 +1,16 @@
+#ifndef __Platform_HPP__
+#define __Platform_HPP__
+
+
+#ifdef _MSC_VER
+
+#include "Platform_Win32.hpp"
+
+//#elif LINUX
+
+//#define "Platform_Linux.hpp
+
+#endif /* _MSC_VER */
+
+
+#endif /* __Platform_HPP__ */
--- gds/Platform_Win32.hpp
+++ gds/Platform_Win32.hpp
@@ -6,9 +6,18 @@
 #include <stdlib.h>
 
 
-//#ifdef _MSC_VER
+//#define strcpy      strcpy_s
+#define strncpy     strncpy_s
+#define strcat      strcat_s
+#define sscanf      sscanf_s
+#define fscanf      fscanf_s
+#define sprintf     sprintf_s
+#define _snprintf   _snprintf_s
+//#define _vsnprintf _vsnprintf_for_win32 // error
+//#define getcwd      _getcwd
+//#define chdir       _chdir
 #define fopen fopen_for_win32
-//#endif /* _MSC_VER */
+
 
 
 inline FILE *fopen_for_win32( const char *filename, const char *mode )
@@ -18,18 +27,17 @@ inline FILE *fopen_for_win32( const char *filename, const char *mode )
 	return fp;
 }
 
-
-//#define strcpy      strcpy_s
-#define strncpy     strncpy_s
-#define strcat      strcat_s
-#define sscanf      sscanf_s
-#define fscanf      fscanf_s
-#define sprintf     sprintf_s
-#define _snprintf   _snprintf_s
-#define _vsnprintfs _vsnprintf_s
-//#define getcwd      _getcwd
-//#define chdir       _chdir
-
+/*
+inline int _vsnprintf_for_win32(
+	char *buffer,
+	size_t count,
+	const char *format,
+	va_list argptr )
+{
+	size_t size_of_buffer = 0xFFFF; // FIXME: find a way to avoid buffer overrun
+	return _vsnprintf_s( buffer, size_of_buffer, count, format, argptr );
+}
+*/
 
 
 #endif /* __Platform_Win32_HPP__ */
