b3869f25d85d0007bd28b4da79763bc04b81c5d2
blue <blue@b80c8c75-5753-0c4f-8f2c-6ef837923b1e>
2009-11-29 08:56:41 +0000

    added: ConvexVolume to 3DMath module.

---
+++ gds/3DMath/ConvexVolume.hpp
@@ -0,0 +1,37 @@
+#ifndef __ConvexVolume_HPP__
+#define __ConvexVolume_HPP__
+
+
+#include <vector>
+#include <gds/3DMath/Plane.hpp>
+
+
+class ConvexVolume
+{
+public:
+
+	/// List of planes that composes the convex shape.
+	/// The planes face outward.
+	std::vector<SPlane> planes;
+
+public:
+
+	inline bool IsPointInside( const Vector3& rvPoint ) const
+	{
+		if( this->planes.size() < 4)
+			return false;
+
+		const size_t num_planes = this->planes.size();
+		for( size_t i=0; i<num_planes; i++ )
+		{
+			if( 0 < this->planes[i].GetDistanceFromPoint( rvPoint ) )
+				return false;
+		}
+
+		return true;
+	}
+};
+
+
+
+#endif /* __ConvexVolume_HPP__ */
