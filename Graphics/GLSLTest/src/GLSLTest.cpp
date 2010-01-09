32c285c44d24a2158ac59797e732a083bdce1ef9
blue <blue@1b90e699-09eb-4805-8378-d520347fdb25>
2010-01-09 01:00:25 +0000

    added: source and project files of GLSLTest

---
+++ Graphics/GLSLTest/src/GLSLTest.cpp
@@ -0,0 +1,413 @@
+#include "GLSLTest.hpp"
+#include <gds/Graphics.hpp>
+#include <gds/Graphics/AsyncResourceLoader.hpp>
+#include <gds/Graphics/Font/BitstreamVeraSansMono_Bold_256.hpp>
+#include <gds/Support/Timer.hpp>
+#include <gds/Support/Profile.hpp>
+#include <gds/Support/Macro.h>
+#include <gds/GUI.hpp>
+
+using namespace std;
+using namespace boost;
+
+
+static int gs_TextureMipLevels = 1;
+
+
+extern CGraphicsTestBase *CreateTestInstance()
+{
+	return new CGLSLTest();
+}
+
+
+extern const std::string GetAppTitle()
+{
+	return string("GLSLTest");
+}
+
+
+static const CSimpleBitmapFontData sg_NullFontData;
+
+const CSimpleBitmapFontData& GetBuiltinFontData( const string& builtin_font_name )
+{
+	if( builtin_font_name == "BitstreamVeraSansMono-Bold-256" )
+	{
+		return g_BitstreamVeraSansMono_Bold_256;
+	}
+	else
+		return sg_NullFontData;
+}
+
+
+CTestMeshHolder::CTestMeshHolder()
+:
+m_LoadingStyle(LOAD_SYNCHRONOUSLY),
+m_Pose(Matrix34Identity())
+{
+}
+
+
+CTestMeshHolder::CTestMeshHolder( const std::string& filepath, LoadingStyleName loading_style, const Matrix34& pose )
+:
+m_LoadingStyle(loading_style),
+m_Pose(pose)
+{
+	m_MeshDesc.ResourcePath = filepath;
+
+	if( loading_style == LOAD_MESH_AND_TEX_TOGETHER )
+	{
+		m_MeshDesc.LoadingMode = CResourceLoadingMode::ASYNCHRONOUS;
+		m_MeshDesc.LoadOptionFlags = MeshLoadOption::LOAD_TEXTURES_ASYNC;
+	}
+	else if( loading_style == LOAD_MESH_AND_TEX_SEPARATELY )
+	{
+		m_MeshDesc.LoadingMode = CResourceLoadingMode::SYNCHRONOUS;
+		m_MeshDesc.LoadOptionFlags = MeshLoadOption::DO_NOT_LOAD_TEXTURES;
+		m_Handle.Load( m_MeshDesc );
+	}
+	else if( loading_style == LOAD_SYNCHRONOUSLY )
+	{
+		m_MeshDesc.LoadingMode = CResourceLoadingMode::SYNCHRONOUS;
+		m_Handle.Load( m_MeshDesc );
+	}
+//	else
+//	{
+//	}
+}
+
+
+void CTestMeshHolder::Load()
+{
+	m_Handle.Load( m_MeshDesc );
+}
+
+
+
+CGLSLTest::CGLSLTest()
+{
+	m_MeshTechnique.SetTechniqueName( "NoLighting" );
+
+	SetBackgroundColor( SFloatRGBAColor( 0.2f, 0.2f, 0.5f, 1.0f ) );
+
+//	m_TestAsyncLoading = true;
+
+//	GraphicsResourceManager().AllowAsyncLoading( m_TestAsyncLoading );
+
+
+	g_Camera.SetPosition( Vector3( 0, 1, -120 ) );
+//	g_Camera.SetPosition( Vector3( 0, 520, 120 ) );
+}
+
+
+CGLSLTest::~CGLSLTest()
+{
+}
+
+
+void CGLSLTest::CreateSampleUI()
+{
+}
+
+
+bool CGLSLTest::InitShader()
+{
+	// initialize shader
+/*	bool shader_loaded = m_Shader.Load( "shaders/glsl_test.vert|shaders/glsl_test.frag" );
+	
+	if( !shader_loaded )
+		return false;
+*/
+	m_pGLProgram = shared_ptr<CGLProgram>( new CGLProgram );
+	m_pGLProgram->LoadShaderFromFile( "shaders/shader.vert|shaders/shader.frag" );
+/*
+	CShaderLightManager *pShaderLightMgr = m_Shader.GetShaderManager()->GetShaderLightManager().get();
+
+	CHemisphericDirectionalLight light;
+	light.Attribute.UpperDiffuseColor.SetRGBA( 1.0f, 1.0f, 1.0f, 1.0f );
+	light.Attribute.LowerDiffuseColor.SetRGBA( 0.1f, 0.1f, 0.1f, 1.0f );
+	light.vDirection = Vec3GetNormalized( Vector3( -1.0f, -1.8f, -0.9f ) );
+
+//	pShaderLightMgr->SetLight( 0, light );
+//	pShaderLightMgr->SetDirectionalLightOffset( 0 );
+//	pShaderLightMgr->SetNumDirectionalLights( 1 );
+	pShaderLightMgr->SetHemisphericDirectionalLight( light );
+*/
+	Matrix44 matProj
+		= Matrix44PerspectiveFoV_LH( (float)PI / 4, 640.0f / 480.0f, 0.1f, 500.0f );
+
+	if( m_Shader.GetShaderManager() )
+		m_Shader.GetShaderManager()->SetProjectionTransform( matProj );
+
+	if( m_pGLProgram )
+		m_pGLProgram->SetProjectionTransform( matProj );
+
+	return true;
+}
+
+
+int CGLSLTest::Init()
+{
+//	m_pFont = shared_ptr<CFontBase>( new CFont( "ÇlÇr ÉSÉVÉbÉN", 6, 12 ) );
+//	m_pFont = shared_ptr<CFontBase>( new CFont( "Bitstream Vera Sans Mono", 16, 16 ) );
+	shared_ptr<CTextureFont> pTexFont( new CTextureFont );
+	pTexFont->InitFont( GetBuiltinFontData( "BitstreamVeraSansMono-Bold-256" ) );
+	pTexFont->SetFontSize( 8, 16 );
+	m_pFont = pTexFont;
+
+	m_vecMesh.push_back( CTestMeshHolder() );
+	shared_ptr<CBoxMeshGenerator> pBoxMeshGenerator( new CBoxMeshGenerator() );
+	pBoxMeshGenerator->SetEdgeLengths( Vector3(1,1,1) );
+	pBoxMeshGenerator->SetDiffuseColor( SFloatRGBAColor::White() );
+	m_vecMesh.back().m_MeshDesc.pMeshGenerator = pBoxMeshGenerator;
+//	m_vecMesh.back().m_MeshDesc.OptionFlags |= GraphicsResourceOption::DONT_SHARE;
+	m_vecMesh.back().Load();
+
+/*
+	m_MeshTechnique.SetTechniqueName( "NoLighting" );
+	m_DefaultTechnique.SetTechniqueName( "NoShader" );
+*/
+
+/*	string mesh_file[] =
+	{
+		"./models/sample_level_00.msh", // manually load textures
+		"./models/FlakySlate.msh",      // load mesh and texture asnchronously
+		"./models/HighAltitude.msh",
+		"./models/RustPeel.msh",
+		"./models/SmashedGrayMarble.msh"
+	};
+
+	BOOST_FOREACH( const string& filepath, mesh_file )
+	{
+		m_vecMesh.push_back( CMeshObjectHandle() );
+
+		CMeshResourceDesc desc;
+		desc.ResourcePath = filepath;
+
+		if( m_TestAsyncLoading )
+			desc.LoadOptionFlags = MeshLoadOption::DO_NOT_LOAD_TEXTURES;
+
+		m_vecMesh.back().Load( desc );
+	}
+*/
+/*
+	m_vecMesh.push_back( CTestMeshHolder( "./models/sample_level_00.msh",   CTestMeshHolder::LOAD_MESH_AND_TEX_SEPARATELY, Matrix34( Vector3(0,0,0), Matrix33Identity() ) ) );
+	m_vecMesh.push_back( CTestMeshHolder( "./models/FlakySlate.msh",        CTestMeshHolder::LOAD_MESH_AND_TEX_TOGETHER,   Matrix34( Vector3( 25,1, 100), Matrix33Identity() ) ) );
+	m_vecMesh.push_back( CTestMeshHolder( "./models/HighAltitude.msh",      CTestMeshHolder::LOAD_MESH_AND_TEX_SEPARATELY,   Matrix34( Vector3(-25,1, 100), Matrix33Identity() ) ) );
+//	m_vecMesh.push_back( CTestMeshHolder( "./models/RustPeel.msh",          CTestMeshHolder::LOAD_MESH_AND_TEX_TOGETHER,   Matrix34( Vector3( 25,1,-100), Matrix33Identity() ) ) );
+//	m_vecMesh.push_back( CTestMeshHolder( "./models/SmashedGrayMarble.msh", CTestMeshHolder::LOAD_MESH_AND_TEX_TOGETHER,   Matrix34( Vector3(-25,1,-100), Matrix33Identity() ) ) );
+*/
+
+	// init shader
+	InitShader();
+
+//	if( m_TestAsyncLoading )
+//		CreateCachedResources();
+
+	return 0;
+}
+
+
+//void CGLSLTest::LoadTexturesAsync()
+void CGLSLTest::LoadResourcesAsync( CTestMeshHolder& holder )
+{/*
+	const Vector3 vCamPos = g_Camera.GetPosition();
+
+//	if( m_vecMesh.size() == 0 )
+//		return;
+
+//	CD3DXMeshObjectBase *pMesh = m_vecMesh[0].GetMesh().get();
+
+	if( holder.m_LoadingStyle == CTestMeshHolder::LOAD_MESH_AND_TEX_TOGETHER
+	 && Vec3Length( holder.m_Pose.vPosition - vCamPos ) < 25.0f
+	 && holder.m_Handle.GetEntryState() == GraphicsResourceState::RELEASED )
+	{
+		// asynchronously load the mesh and its texture(s)
+		holder.m_Handle.Load( holder.m_MeshDesc );
+	}
+
+	if( holder.m_LoadingStyle == CTestMeshHolder::LOAD_MESH_AND_TEX_TOGETHER )
+		return;
+
+	CD3DXMeshObjectBase *pMesh = holder.m_Handle.GetMesh().get();
+	if( !pMesh )
+		return;
+
+	const int num_materials = pMesh->GetNumMaterials();
+	for( int i=0; i<num_materials; i++ )
+	{
+		AABB3 aabb = pMesh->GetAABB(i);
+
+		AABB3 world_aabb;
+		Vector3 vWorldPos = holder.m_Pose.vPosition;
+		world_aabb.vMin = vWorldPos + aabb.vMin - Vector3(1,1,1) * 3.0f;
+		world_aabb.vMax = vWorldPos + aabb.vMax + Vector3(1,1,1) * 3.0f;
+
+		Vector3 vDist = world_aabb.GetCenterPosition() - vCamPos;
+		const float fDist = Vec3Length(vDist);
+		CTextureHandle& tex = pMesh->GetTexture( i, 0 );
+
+		if( fDist < 25.0f // dist between cam position and the triangle set is less than 25m - for small parts
+		 || world_aabb.IsPointInside(vCamPos) ) // cam position is included in the AABB of the triangle set - for large parts
+		{
+//			if( tex.IsLoaded() == false )
+			if( tex.GetEntryState() == GraphicsResourceState::RELEASED
+			 && !tex.GetEntry() )
+			{
+				CTextureResourceDesc desc = pMesh->GetMaterial(i).TextureDesc[0];
+				desc.LoadingMode = CResourceLoadingMode::ASYNCHRONOUS;
+				desc.Format = TextureFormat::A8R8G8B8;
+				desc.MipLevels = gs_TextureMipLevels;
+
+				tex.Load( desc );
+			}
+		}
+		else if( 30.0f < fDist 
+		 && !world_aabb.IsPointInside(vCamPos) )
+		{
+			if( tex.GetEntryState() == GraphicsResourceState::LOADED )
+				tex.Release();
+		}
+	}*/
+}
+
+
+void CGLSLTest::Update( float dt )
+{
+	if( m_pSampleUI )
+		m_pSampleUI->Update( dt );
+
+/*
+	if( true //m_TestAsyncLoading/ )
+	{
+//		LoadTexturesAsync();
+
+		BOOST_FOREACH( CTestMeshHolder& holder, m_vecMesh )
+		{
+			//if( holder.m_LoadingStyle != CTestMeshHolder::LOAD_SYNCHRONOUSLY )
+				LoadResourcesAsync( holder );
+		}
+	}
+
+	if( !GraphicsResourceManager().IsAsyncLoadingAllowed() )
+	{
+		// async loading is not enabled
+		// - The primary thread (this thread) loads the resources from the disk/memory.
+		AsyncResourceLoader().ProcessResourceLoadRequest();
+	}*/
+}
+
+
+void CGLSLTest::RenderMeshes()
+{
+	LPDIRECT3DDEVICE9 pd3dDevice = DIRECT3D9.GetDevice();
+
+//	pd3dDevice->SetRenderState( D3DRS_ZENABLE, D3DZB_TRUE );
+	GraphicsDevice().SetRenderState( RenderStateType::DEPTH_TEST, true );
+
+	CShaderManager *pShaderManager = m_pGLProgram.get();
+//	CShaderManager *pShaderManager = m_Shader.GetShaderManager();
+	if( !pShaderManager )
+		return;
+
+	// render the scene
+
+	pShaderManager->SetViewerPosition( g_Camera.GetPosition() );
+
+	ShaderManagerHub.PushViewAndProjectionMatrices( g_Camera );
+
+	pShaderManager->SetTechnique( m_MeshTechnique );
+//	BOOST_FOREACH( CMeshObjectHandle& mesh, m_vecMesh )
+	BOOST_FOREACH( CTestMeshHolder& holder, m_vecMesh )
+	{
+		if( holder.m_Handle.GetEntryState() == GraphicsResourceState::LOADED )
+		{
+			// set world transform
+			const Matrix34 mesh_world_pose = holder.m_Pose;
+//			FixedPipelineManager().SetWorldTransform( mesh_world_pose );
+			pShaderManager->SetWorldTransform( mesh_world_pose );
+
+			shared_ptr<CBasicMesh> pMesh = holder.m_Handle.GetMesh();
+
+			if( pMesh )
+				pMesh->Render( *pShaderManager );
+		}
+	}
+
+	ShaderManagerHub.PopViewAndProjectionMatrices_NoRestore();
+}
+
+
+void CGLSLTest::Render()
+{
+	PROFILE_FUNCTION();
+
+//	AsyncResourceLoader().ProcessGraphicsDeviceRequests();
+
+	RenderMeshes();
+
+	if( m_pSampleUI )
+		m_pSampleUI->Render();
+
+//	AsyncResourceLoader().ProcessGraphicsDeviceRequests();
+
+//	GraphicsResourceManager().GetStatus( GraphicsResourceType::Texture, m_TextBuffer );
+
+	Vector2 vTopLeft(     (float)GetWindowWidth() / 4,  (float)16 );
+	Vector2 vBottomRight( (float)GetWindowWidth() - 16, (float)GetWindowHeight() * 3 / 2 );
+	C2DRect rect( vTopLeft, vBottomRight, 0x50000000 );
+	rect.Draw();
+
+	m_pFont->DrawText( m_TextBuffer, vTopLeft );
+
+	Vector3 vCamPos = g_Camera.GetPosition();
+	m_pFont->DrawText(
+		fmt_string( "x: %f\ny: %f\nz: %f\n", vCamPos.x, vCamPos.y, vCamPos.z ),
+		Vector2( 20, 300 ) );
+}
+
+
+
+
+void CGLSLTest::HandleInput( const SInputData& input )
+{
+	if( m_pUIInputHandler )
+	{
+//		CInputHandler::ProcessInput() does not take const SInputData&
+		SInputData input_copy = input;
+		m_pUIInputHandler->ProcessInput( input_copy );
+
+		if( m_pUIInputHandler->PrevInputProcessed() )
+			return;
+	}
+
+	switch( input.iGICode )
+	{
+	case GIC_F12:
+		if( input.iType == ITYPE_KEY_PRESSED )
+		{
+//			SaveTexturesAsImageFiles();
+		}
+		break;
+	case GIC_SPACE:
+	case GIC_ENTER:
+		if( input.iType == ITYPE_KEY_PRESSED )
+		{
+//			m_pSampleUI->GetDialog(UIID_DLG_RESOLUTION)->Open();
+		}
+		break;
+	default:
+		break;
+	}
+}
+
+
+void CGLSLTest::ReleaseGraphicsResources()
+{
+//	m_pSampleUI.reset();
+}
+
+
+void CGLSLTest::LoadGraphicsResources( const CGraphicsParameters& rParam )
+{
+//	CreateSampleUI();
+}
---
+++ Graphics/GLSLTest/src/GLSLTest.hpp
@@ -0,0 +1,126 @@
+#ifndef  __GLSLTest_H__
+#define  __GLSLTest_H__
+
+
+#include <vector>
+#include <boost/foreach.hpp>
+#include <boost/shared_ptr.hpp>
+using namespace boost;
+
+#include <gds/3DMath/Matrix34.hpp>
+#include <gds/Graphics/fwd.hpp>
+#include <gds/Graphics/GraphicsComponentCollector.hpp>
+#include <gds/Graphics/MeshObjectHandle.hpp>
+#include <gds/Graphics/ShaderHandle.hpp>
+#include <gds/Graphics/Shader/ShaderTechniqueHandle.hpp>
+#include <gds/Input/fwd.hpp>
+#include <gds/Input.hpp>
+#include <gds/GUI/fwd.hpp>
+
+#include <gds/Graphics/OpenGL/Shader/GLProgram.hpp>
+
+#include "../../../_Common/GraphicsTestBase.hpp"
+
+
+class CTestMeshHolder
+{
+public:
+	CMeshObjectHandle m_Handle;
+	Matrix34 m_Pose;
+
+	CMeshResourceDesc m_MeshDesc;
+
+	enum LoadingStyleName
+	{
+		LOAD_SYNCHRONOUSLY,
+		LOAD_MESH_AND_TEX_TOGETHER,
+		LOAD_MESH_AND_TEX_SEPARATELY,
+		NUM_LOADING_STYLES
+	};
+
+	LoadingStyleName m_LoadingStyle;
+
+	CTestMeshHolder();
+
+	CTestMeshHolder( const std::string& filepath, LoadingStyleName loading_style, const Matrix34& pose );
+
+	void Load();
+};
+
+
+class CGLSLTest : public CGraphicsTestBase, public CGraphicsComponent
+{
+	enum UIID
+	{
+		UIID_DLG_ROOT = 1000,
+		UIID_DLG_SLIDERS,
+		UIID_DLG_RESOLUTION,
+		UIID_LBX_RESOLUTION,
+		UIID_DLG_LISTBOXGROUP,
+		UIID_OTHER
+	};
+
+	enum Params
+	{
+		TEXT_BUFFER_SIZE = 4096
+	};
+
+//	std::vector<CMeshObjectHandle> m_vecMesh;
+	std::vector<CTestMeshHolder> m_vecMesh;
+
+	CShaderHandle m_Shader;
+
+	CShaderTechniqueHandle m_MeshTechnique;
+
+	shared_ptr<CInputHandler_Dialog> m_pUIInputHandler;
+
+	boost::shared_ptr<CFontBase> m_pFont;
+
+	CInputHandlerSharedPtr m_pInputHandler;
+
+	CGM_DialogManagerSharedPtr m_pSampleUI;
+
+	boost::shared_ptr<CGLProgram> m_pGLProgram;
+
+//	bool m_TestAsyncLoading;
+
+	char m_TextBuffer[TEXT_BUFFER_SIZE];
+
+private:
+
+	void CreateSampleUI();
+
+	bool InitShader();
+
+	void CreateCachedResources();
+
+	void RenderMeshes();
+
+//	void LoadTexturesAsync();
+	void LoadResourcesAsync( CTestMeshHolder& holder );
+//	void SaveTexturesAsImageFiles();
+
+public:
+
+	CGLSLTest();
+
+	~CGLSLTest();
+
+	int Init();
+
+	void Release() {};
+
+	void Update( float dt );
+
+	void Render();
+
+//	virtual void RenderBase();
+
+	virtual void HandleInput( const SInputData& input );
+
+	void ReleaseGraphicsResources();
+	void LoadGraphicsResources( const CGraphicsParameters& rParam );
+};
+
+
+#endif /* __GLSLTest_H__ */
