a26cd3d86b914d67e67d4ed500db1b8c8f52a658
blue <blue@b80c8c75-5753-0c4f-8f2c-6ef837923b1e>
2010-03-08 13:48:00 +0000

    updated: PyModule_task.cpp - Replaced g_pAppBase with g_pGameAppBase.

--- gds/Task/PyModule_TaskMethods.cpp
+++ gds/Task/PyModule_TaskMethods.cpp
@@ -14,9 +14,9 @@ using namespace std;
 
 inline static bool IsTaskAvailable()
 {
-	if( g_pAppBase
-	 && g_pAppBase->GetTaskManager()
-	 && g_pAppBase->GetTaskManager()->GetCurrentTask() )
+	if( g_pGameAppBase
+	 && g_pGameAppBase->GetTaskManager()
+	 && g_pGameAppBase->GetTaskManager()->GetCurrentTask() )
 		return true;
 	else
 		return false;
@@ -41,7 +41,7 @@ PyObject* RequestTaskTransition( PyObject* self, PyObject* args )
 
 	RETURN_PYNONE_IF_NO_TASK()
 
-	g_pAppBase->GetTaskManager()->GetCurrentTask()->RequestTaskTransition( task_name, priority, delay_in_sec );
+	g_pGameAppBase->GetTaskManager()->GetCurrentTask()->RequestTaskTransition( task_name, priority, delay_in_sec );
 
     Py_INCREF( Py_None );
 	return Py_None;
