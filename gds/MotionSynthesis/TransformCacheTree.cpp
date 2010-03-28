bae96e3c994f119a7a8d0d81bfcc56436e58c678
blue <blue@b80c8c75-5753-0c4f-8f2c-6ef837923b1e>
2010-03-28 13:35:28 +0000

    added: an early draft version of the following classes to the MotionSynthesis module. - CTransformCacheNode - CTransformCacheTree

---
+++ gds/MotionSynthesis/TransformCacheTree.cpp
@@ -0,0 +1,208 @@
+#include "TransformCacheTree.hpp"
+#include "Skeleton.hpp"
+#include "MotionPrimitive.hpp"
+
+
+using namespace std;
+
+
+namespace msynth
+{
+
+
+//=====================================================================
+// CTransformCacheNode
+//=====================================================================
+
+CTransformCacheNode *CTransformCacheNode::GetNode( const std::string& name )
+{
+	if( m_Name == name )
+		return this;
+
+	for( int i=0; i<m_NumChildren; i++ )
+	{
+		CTransformCacheNode *pNode = m_pChildren[i].GetNode( name );
+		if( pNode )
+			return pNode;
+	}
+
+	return NULL;
+}
+
+
+void CTransformCacheNode::CreateTransformCacheNodeTree_r( const CBone& bone )
+{
+	m_Name = bone.GetName();
+
+	// found the name in the list
+	m_Active = true;
+
+	m_NumChildren = bone.GetNumChildren();
+
+	if( m_NumChildren == 0 )
+		return;
+
+	m_pChildren = new CTransformCacheNode [m_NumChildren];
+//	children.resize( bone.GetNumChildren() );
+
+	for( int i=0; i<m_NumChildren; i++ )
+	{
+		m_pChildren[i].CreateTransformCacheNodeTree_r( bone.GetChild(i) );
+	}
+}
+
+
+bool CTransformCacheNode::AreAllChildrenInactive() const
+{
+	if( m_NumChildren == 0 && !m_Active )
+		return true;
+	else
+	{
+		for( int i=0; i<m_NumChildren; i++ )
+		{
+			bool res = AreAllChildrenInactive();
+			if( !res )
+				return false;
+		}
+
+		return true;
+	}
+}
+
+
+bool CTransformCacheNode::AreAllChildrenLeafAndInactive() const
+{
+	for( int i=0; i<m_NumChildren; i++ )
+	{
+		if( m_pChildren[i].m_NumChildren && m_pChildren[i].m_Active )
+			continue;
+		else
+			return false;
+	}
+
+	return true;
+}
+
+
+bool CTransformCacheNode::UpdateActiveNodes( const std::set<std::string>& target_bone_names )
+{
+	m_Active = true;
+
+	for( int i=0; i<m_NumChildren; i++ )
+	{
+		bool are_children_active = m_pChildren[i].UpdateActiveNodes( target_bone_names );
+
+		// Return true if at least one of the child nodes is active
+		if( are_children_active )
+			return true;
+	}
+
+	set<string>::const_iterator itr = target_bone_names.find( m_Name );
+
+	m_Active = (itr != target_bone_names.end()) ? true : false;
+
+	return m_Active;
+}
+
+
+void CTransformCacheNode::RemoveInactiveNodes( const std::set<std::string>& target_bone_names )
+{
+	for( int i=0; i<m_NumChildren; i++ )
+	{
+		m_pChildren[i].RemoveInactiveNodes( target_bone_names );
+	}
+
+	if( AreAllChildrenLeafAndInactive() )
+	{
+		ReleaseChildren();
+	}
+
+	if( m_NumChildren == 0 )
+	{
+		set<string>::const_iterator itr = target_bone_names.find( m_Name );
+		if( itr == target_bone_names.end() )
+			m_Active = false;
+	}
+}
+
+
+void CTransformCacheNode::CalculateWorldTransforms( const Matrix34& parent_transform, const CBone& bone, const CTransformNode& transform_node )
+{
+//	if( !cache_node.m_Active )
+//		return;
+
+	bone.CalculateWorldTransform( m_WorldTransform, parent_transform, transform_node );
+
+	const int num_children = take_min( m_NumChildren, bone.GetNumChildren(), transform_node.GetNumChildren() );
+	for( int i=0; i<num_children; i++ )
+	{
+		if( m_pChildren[i].m_Active )
+			m_pChildren[i].CalculateWorldTransforms( m_WorldTransform, bone.GetChild(i), transform_node.GetChildNode(i) );
+	}
+}
+
+
+
+//=====================================================================
+// CTransformCacheTree
+//=====================================================================
+
+void CTransformCacheTree::UpdateActiveNodes( const std::set<std::string>& target_bone_names )
+{
+	m_pRootNode->UpdateActiveNodes( target_bone_names );
+}
+
+
+void CTransformCacheTree::RemoveInactiveNodes( const std::set<std::string>& target_bone_names )
+{
+	m_pRootNode->RemoveInactiveNodes( target_bone_names );
+}
+
+
+void CTransformCacheTree::Create( const CSkeleton& skeleton )
+{
+	m_pRootNode->CreateTransformCacheNodeTree_r( skeleton.GetRootBone() );
+}
+
+
+void CTransformCacheTree::CalculateWorldTransforms( const CSkeleton& skeleton, const CKeyframe& keyframe, const Matrix34& parent_transform )
+{
+	m_pRootNode->CalculateWorldTransforms( parent_transform, skeleton.GetRootBone(), keyframe.GetRootNode() );
+}
+
+
+} // namespace msynth
+
+/*
+void CTransformCacheNode::CreateTransformCacheNodeTree_r( const CBone& bone, const std::set<std::string>& target_bone_names )
+{
+	set<string>::const_iterator itr = target_bone_names.find( bone.GetName() );
+	if( target_bone_names.end() == itr )
+		return;
+
+	m_Name = bone.GetName();
+
+	// found the name in the list
+	m_Active = true;
+
+	m_NumChildren = bone.GetNumChildren();
+
+	if( m_NumChildren == 0 )
+		return;
+
+	m_pChildren = new CTransformCacheNode [m_NumChildren];
+//	children.resize( bone.GetNumChildren() );
+
+	for( int i=0; i<m_NumChildren; i++ )
+	{
+		m_pChildren[i].CreateTransformCacheNodeTree_r( bone.GetChild(i), target_bone_names );
+	}
+}
+*/
+
+/*
+void CTransformCacheTree::Create( CSkeleton& skeleton, const std::set<std::string>& target_bone_names )
+{
+	m_pRootNode->CreateTransformCacheNodeTree_r( skeleton.GetRootBone(), target_bone_names );
+}
+*/
---
+++ gds/MotionSynthesis/TransformCacheTree.hpp
@@ -0,0 +1,118 @@
+#ifndef __TransformCacheTree_HPP__
+#define __TransformCacheTree_HPP__
+
+
+#include <set>
+#include <string>
+#include "gds/3DMath/Matrix34.hpp"
+#include "gds/Support/memory_helpers.hpp"
+#include "fwd.hpp"
+
+
+namespace msynth
+{
+
+
+class CTransformCacheNode
+{
+	std::string m_Name;
+
+	Matrix34 m_WorldTransform;
+
+//	std::vector<CTransformCacheNode> children;
+	CTransformCacheNode *m_pChildren;
+
+	int m_NumChildren;
+
+	// true: calculate the world transform for this node and store the result to m_WorldTransform
+	// false: do not calculate the world transform for this and all the child nodes
+	bool m_Active;
+
+private:
+
+	bool AreAllChildrenInactive() const;
+
+	bool AreAllChildrenLeafAndInactive() const;
+
+public:
+
+	CTransformCacheNode()
+		:
+	m_pChildren(NULL),
+	m_NumChildren(0),
+	m_WorldTransform( Matrix34Identity() ),
+	m_Active(true)
+	{}
+
+	~CTransformCacheNode()
+	{
+		ReleaseChildren();
+	}
+
+	void ReleaseChildren()
+	{
+		SafeDeleteArray( m_pChildren );
+		m_NumChildren = 0;
+	}
+
+	CTransformCacheNode *GetNode( const std::string& name );
+
+//	void CreateTransformCacheNodeTree_r( const CBone& bone, const std::set<std::string>& target_bone_names );
+
+	void CreateTransformCacheNodeTree_r( const CBone& bone );
+
+	bool UpdateActiveNodes( const std::set<std::string>& target_bone_names );
+
+	void RemoveInactiveNodes( const std::set<std::string>& target_bone_names );
+
+	void CalculateWorldTransforms( const Matrix34& parent_transform, const CBone& bone, const CTransformNode& transform_node );
+
+	void GetWorldTransform( Matrix34& dest ) const { dest = m_WorldTransform; }
+};
+
+
+
+// not copiable
+class CTransformCacheTree
+{
+	CTransformCacheNode *m_pRootNode;
+
+public:
+
+	CTransformCacheTree()
+		:
+	m_pRootNode( new CTransformCacheNode() )
+	{}
+
+	~CTransformCacheTree() { Release(); }
+
+	void Release()
+	{
+		SafeDelete( m_pRootNode );
+	}
+	
+	void Create( const CSkeleton& skeleton );
+
+/*	void Create( CSkeleton& skeleton, const std::set<std::string>& target_bone_names );
+
+	void Create( CSkeleton& skeleton, const std::vector<std::string>& target_bone_names )
+	{
+		std::set<std::string> names;
+		names.insert( target_bone_names.begin(), target_bone_names.end() );
+		Create( skeleton, names );
+	}*/
+
+	void UpdateActiveNodes( const std::set<std::string>& target_bone_names );
+
+	void RemoveInactiveNodes( const std::set<std::string>& target_bone_names );
+
+	CTransformCacheNode *GetNode( const std::string& name ) { return m_pRootNode->GetNode( name ); }
+
+	void CalculateWorldTransforms( const CSkeleton& skeleton, const CKeyframe& keyframe, const Matrix34& parent_transform = Matrix34Identity() );
+};
+
+
+} // namespace msynth
+
+
+#endif /* __TransformCacheTree_HPP__ */
