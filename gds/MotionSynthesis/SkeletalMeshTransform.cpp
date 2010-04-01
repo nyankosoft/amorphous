c5a1d4845f26f7c79d0600af59a809bb8c0c5a13
blue <blue@b80c8c75-5753-0c4f-8f2c-6ef837923b1e>
2010-04-01 16:58:51 +0000

    updated: MotionSynthesis/SkeletalMeshTransform.hpp & cpp

--- gds/MotionSynthesis/SkeletalMeshTransform.cpp
+++ gds/MotionSynthesis/SkeletalMeshTransform.cpp
@@ -37,4 +37,63 @@ void UpdateMeshBoneTransforms_r( const msynth::CBone& bone,
 }
 
 
+void UpdateMeshBoneTransforms_r( const msynth::CTransformNodeMap& map_node,
+                                 const msynth::CTransformNode& node,
+								 CSkeletalMesh& mesh )
+{
+	// find the matrix index from the bone name (slow).
+	int index = map_node.m_DestIndex;
+//	if( index == -1 )
+//		return;
+
+	if( 0 <= index )
+	{
+		Matrix34 local_transform;
+		local_transform.vPosition = node.GetLocalTranslation();
+		local_transform.matOrient = node.GetLocalRotationQuaternion().ToRotationMatrix();
+		mesh.SetLocalTransformToCache( index, local_transform );
+	}
+
+	const int num_child_bones = map_node.GetNumChildren();
+	const int num_child_nodes = node.GetNumChildren();
+	const int num_children = take_min( num_child_bones, num_child_nodes );
+//	const int num_children = node.GetNumChildren();
+	for( int i=0; i<num_children; i++ )
+	{
+		UpdateMeshBoneTransforms_r(
+			map_node.GetChild(i),
+			node.GetChildNode(i),
+			mesh );
+	}
+}
+
+
+void CreateTransformMapTree_r( const msynth::CBone& src_bone,
+							  msynth::CTransformNodeMap& map_node,
+							  const CSkeletalMesh& mesh )
+{
+	// find the matrix index from the bone name (slow).
+	map_node.m_DestIndex = mesh.GetBoneMatrixIndexByName( src_bone.GetName() );
+
+	const int num_child_bones = src_bone.GetNumChildren();
+//	const int num_child_nodes = node.GetNumChildren();
+	const int num_children = num_child_bones;//take_min( num_child_bones, num_child_nodes );
+
+	map_node.m_Children.resize( num_children );
+	for( int i=0; i<num_children; i++ )
+	{
+		CreateTransformMapTree_r(
+			src_bone.GetChild(i),
+			map_node.Child(i),
+			mesh );
+	}
+}
+
+
+void CreateTransformMapTree( const msynth::CSkeleton& src_skeleton, msynth::CTransformNodeMap& root_map_node, const CSkeletalMesh& mesh )
+{
+	CreateTransformMapTree_r( src_skeleton.GetRootBone(), root_map_node, mesh );
+}
+
+
 } // namespace msynth
--- gds/MotionSynthesis/SkeletalMeshTransform.hpp
+++ gds/MotionSynthesis/SkeletalMeshTransform.hpp
@@ -12,7 +12,38 @@ namespace msynth
 {
 
 
+class CTransformNodeMap
+{
+public:
+
+	int m_DestIndex;
+
+	std::vector<CTransformNodeMap> m_Children;
+
+public:
+
+	CTransformNodeMap()
+		:
+	m_DestIndex(-1)
+	{}
+
+	int GetNumChildren() const { return (int)m_Children.size(); }
+
+	const CTransformNodeMap& GetChild( int i ) const { return m_Children[i]; }
+
+	CTransformNodeMap& Child( int i ) { return m_Children[i]; }
+};
+
+
+// ---------------------------- Global Functions ----------------------------
+
+/// This is a private function of the msynth module
+/// Client code should not call this.
 void UpdateMeshBoneTransforms_r( const CBone& bone, const CTransformNode& node, CSkeletalMesh& mesh );
+
+/// This is a private function of the msynth module
+/// Client code should not call this.
+void UpdateMeshBoneTransforms_r( const CTransformNodeMap& map_node, const CTransformNode& node, CSkeletalMesh& mesh );
 
 /// Set transforms to mesh bones.
 /// For each bone, search the corresponding mesh bone with CSkeletalMesh::GetBoneMatrixIndexByName() (slow)
@@ -23,6 +54,15 @@ inline void UpdateMeshBoneTransforms( const CKeyframe& keyframe, const CSkeleton
 {
 	UpdateMeshBoneTransforms_r( skeleton.GetRootBone(), keyframe.GetRootNode(), target_skeletal_mesh );
 }
+
+
+inline void UpdateMeshBoneTransforms( const CKeyframe& keyframe, const CTransformNodeMap& root_map_node, CSkeletalMesh& target_skeletal_mesh )
+{
+	UpdateMeshBoneTransforms_r( root_map_node, keyframe.GetRootNode(), target_skeletal_mesh );
+}
+
+
+void CreateTransformMapTree( const CSkeleton& src_skeleton, CTransformNodeMap& root_map_node, const CSkeletalMesh& mesh );
 
 
 } // namespace msynth
