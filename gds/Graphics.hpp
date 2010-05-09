9ff8e470956d951588039bdda9f1eff35dfe5e62
blue <blue@b80c8c75-5753-0c4f-8f2c-6ef837923b1e>
2010-05-09 14:28:01 +0000

    removed: inclusions of Direct3D-specific headers in gds/Graphics.hpp

--- gds/Graphics.hpp
+++ gds/Graphics.hpp
@@ -23,13 +23,7 @@
 #include "Graphics/3DGameMath.hpp"
 #include "Graphics/3DRect.hpp"
 #include "Graphics/Camera.hpp"
-#include "Graphics/D3DMisc.hpp"
-#include "Graphics/Direct3D/Mesh/D3DXMeshObject.hpp"
-#include "Graphics/Direct3D/Mesh/D3DXMeshObjectBase.hpp"
-#include "Graphics/Direct3D/Mesh/D3DXPMeshObject.hpp"
-#include "Graphics/Direct3D/Mesh/D3DXSMeshObject.hpp"
 #include "Graphics/D3DXVec3Copy.hpp"
-#include "Graphics/Direct3D/Direct3D9.hpp"
 #include "Graphics/FloatRGBAColor.hpp"
 #include "Graphics/FloatRGBColor.hpp"
 
