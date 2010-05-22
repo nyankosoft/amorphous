ea48fed8a4be6f1e3663d88cfda21142d556b58a
blue <blue@09f06ba5-967e-c943-bf14-690b5ca779b5>
2010-05-22 02:20:33 +0000

    updated: AutoResourceArchiver - Added a feature to recursively add and compile the resource files found during the compilation. - Properly stops the monitoring thread.

--- AutoResourceArchiver/src/AutoResourceArchiverMain.cpp
+++ AutoResourceArchiver/src/AutoResourceArchiverMain.cpp
@@ -27,15 +27,46 @@ public:
 		}
 	}*/
 
-	Result::Name CreateArchive( const std::string& input_filepath, boost::shared_ptr<IArchiveObjectBase>& pArchive )
+//	Result::Name CreateArchive( const std::string& input_filepath, boost::shared_ptr<IArchiveObjectBase>& pArchive, CResourceArchiverUnitOutput& out )
+	Result::Name CreateArchive( const std::string& input_filepath, const std::string& output_filepath, CResourceArchiverUnitOutput& out )
 	{
 		if( path(input_filepath).extension() != ".lwo" )
 			return Result::INVALID_ARGS;
 
 		C3DMeshModelExportManager_LW lw_mesh_exporter;
-		lw_mesh_exporter.BuildMeshModels( input_filepath );
+		lw_mesh_exporter.BuildMeshModels( input_filepath,
+			  C3DMeshModelBuilder::BOF_OUTPUT_AS_TEXTFILE
+			| C3DMeshModelBuilder::BOF_CHANGE_TEXTURE_FILE_EXTENSIONS_TO_IA );
 
-		return Result::UNKNOWN_ERROR;
+		const int num_built_models = lw_mesh_exporter.GetNumBuiltMeshModelArchives();
+		for( int i=0; i<num_built_models; i++ )
+		{
+			const vector< vector<string> > orig_tex_paths = lw_mesh_exporter.GetOriginalTextureFilepaths( i );
+
+			C3DMeshModelArchive* pArchive = lw_mesh_exporter.GetBuiltMeshModelArchive( i );
+			if( !pArchive )
+				continue;
+
+			const size_t num_materials = pArchive->GetMaterial().size();
+			for( size_t mat=0; mat<num_materials; mat++ )
+			{
+				const CMMA_Material& material = pArchive->GetMaterial()[mat];
+				const size_t num_textures = material.vecTexture.size();
+				for( size_t tex=0; tex<num_textures; tex++ )
+				{
+					if( material.vecTexture[tex].type != CMMA_Texture::FILENAME )
+						continue;
+
+					path mesh_output_path = lw_mesh_exporter.GetOutputFilepath( i );
+
+					const path output_path = path(input_filepath).parent_path() / mesh_output_path.parent_path() / material.vecTexture[tex].strFilename;
+//					const path output_path = path(output_filepath).parent_path() / material.vecTexture[tex].strFilename;
+					out.m_ExtraTargets[ orig_tex_paths[mat][tex] ] = output_path.string();
+				}
+			}
+		}
+
+		return (0 < num_built_models) ? Result::SUCCESS : Result::UNKNOWN_ERROR;
 	}
 };
 
@@ -58,14 +89,23 @@ void Init( CAutoResourceArchiver& ara )
 int main( int argc, char *argv[] )
 {
 	// >>> release
-//	if( argc <= 1 )
-//		return 0;
+	string root_desc_file;
+	if( 1 < argc )
+	{
+		root_desc_file = argv[1];
+	}
+	else
+	{
+		bool res = GetFilename( root_desc_file );
+	}
 
-//	const string root_desc_file( argv[1] );
+	if( root_desc_file.length() == 0 )
+		return 0;
+
 	// <<< release
 
 	// >>> test
-	const string root_desc_file( "../../../GameProjects/FlightGame/resources/images/ss_imgs" );
+//	string root_desc_file( "../../../GameProjects/FlightGame/resources/images/ss_imgs" );
 	// <<< test
 
 	CAutoResourceArchiver ara;
@@ -81,7 +121,11 @@ int main( int argc, char *argv[] )
 	{
 		getline (cin,str);
 		if( str == "quit" )
+		{
+			ara.StopMonitoring();
+			ara.join();
 			break;
+		}
 	}
 
 	ara.SaveBuildInfoToDisk();
