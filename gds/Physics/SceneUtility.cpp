046766331ff367f175b01e8a1e5fa3436f663ad8
blue <blue@b80c8c75-5753-0c4f-8f2c-6ef837923b1e>
2010-05-23 02:43:33 +0000

    added: CSceneUtility to the Physics module. - Provides convenience functions for creating physics actors.

---
+++ gds/Physics/SceneUtility.cpp
@@ -0,0 +1,71 @@
+#include "SceneUtility.hpp"
+#include "gds/Physics.hpp"
+
+using namespace physics;
+
+
+CActor *CSceneUtility::CreateBoxActor( const Vector3& side_lengths, const Matrix34& world_pose, float mass )
+{
+	if( !m_pScene )
+		return NULL;
+
+	CActorDesc actor_desc;
+	actor_desc.WorldPose = world_pose;
+	actor_desc.BodyDesc.fMass = mass;
+
+	CBoxShapeDesc box;
+	box.MaterialIndex = 1;
+	box.vSideLength = side_lengths;
+	actor_desc.vecpShapeDesc.push_back(&box);
+
+	return m_pScene->CreateActor( actor_desc );
+}
+
+
+CActor *CSceneUtility::CreateBoxActor( const Vector3& side_lengths, const Matrix34& world_pose, U32 body_flags )
+{
+	if( !m_pScene )
+		return NULL;
+
+	CActorDesc actor_desc;
+	actor_desc.WorldPose = world_pose;
+	actor_desc.BodyDesc.Flags = body_flags;
+
+	CBoxShapeDesc box;
+	box.MaterialIndex = 0;
+	box.vSideLength = side_lengths;
+	actor_desc.vecpShapeDesc.push_back(&box);
+
+	return m_pScene->CreateActor( actor_desc );
+}
+
+
+CActor *CSceneUtility::CreateStaticBoxActor( const Vector3& side_lengths, const Matrix34& world_pose )
+{
+	return CreateBoxActor( side_lengths, world_pose, (U32)PhysBodyFlag::Static );
+}
+
+
+CActor *CSceneUtility::CreateKinematicBoxActor( const Vector3& side_lengths, const Matrix34& world_pose )
+{
+	return CreateBoxActor( side_lengths, world_pose, (U32)PhysBodyFlag::Kinematic );
+}
+
+
+CActor *CSceneUtility::CreateStaticCapsuleActor( float radius, float length, const Matrix34& world_pose )
+{
+	if( !m_pScene )
+		return NULL;
+
+	CActorDesc actor_desc;
+	actor_desc.WorldPose = world_pose;
+	actor_desc.BodyDesc.Flags = PhysBodyFlag::Static;
+
+	CCapsuleShapeDesc cap;
+	cap.MaterialIndex = 0;
+	cap.fRadius = radius;
+	cap.fLength = length;
+	actor_desc.vecpShapeDesc.push_back(&cap);
+
+	return m_pScene->CreateActor( actor_desc );
+}
---
+++ gds/Physics/SceneUtility.hpp
@@ -0,0 +1,44 @@
+#ifndef __SceneUtility_HPP__
+#define __SceneUtility_HPP__
+
+
+#include "gds/Graphics/fwd.hpp"
+#include "gds/3DMath/Matrix34.hpp"
+#include "gds/Physics/fwd.hpp"
+
+
+namespace physics
+{
+
+
+class CSceneUtility
+{
+	CScene *m_pScene;
+
+	CActor *CreateBoxActor( const Vector3& side_lengths, const Matrix34& world_pose, U32 body_flags );
+
+public:
+
+	CSceneUtility( CScene *pScene )
+		:
+	m_pScene(pScene)
+	{}
+
+	virtual ~CSceneUtility() {}
+
+	CActor *CreateBoxActor( const Vector3& side_lengths, const Matrix34& world_pose, float mass = 1.0f );
+
+	CActor *CreateStaticBoxActor( const Vector3& side_lengths, const Matrix34& world_pose );
+
+	CActor *CreateKinematicBoxActor( const Vector3& side_lengths, const Matrix34& world_pose );
+
+	CActor *CreateStaticCapsuleActor( float radius, float length, const Matrix34& world_pose );
+
+	CActor *CreateStaticSphereActor( Scalar radius, const Matrix34& world_pose );
+};
+
+
+} // namespace physics
+
+
+#endif /* __SceneUtility_HPP__ */
