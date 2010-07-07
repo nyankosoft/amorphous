36ff558fd610fdd5c1a51d6c8075c51e830859e9
blue <blue@1b90e699-09eb-4805-8378-d520347fdb25>
2010-07-07 14:02:21 +0000

    added: MultibyteFontTest

---
+++ Graphics/MultibyteFontTest/src/MultibyteFontTest.hpp
@@ -0,0 +1,98 @@
+#ifndef  __MultibyteFontTest_H__
+#define  __MultibyteFontTest_H__
+
+
+#include "gds/3DMath/Matrix34.hpp"
+#include "gds/Graphics/fwd.hpp"
+#include "gds/Graphics/GraphicsComponentCollector.hpp"
+#include "gds/Graphics/TextureHandle.hpp"
+#include "gds/Graphics/MeshObjectHandle.hpp"
+#include "gds/Graphics/ShaderHandle.hpp"
+#include "gds/Graphics/Shader/ShaderTechniqueHandle.hpp"
+#include "gds/Input/fwd.hpp"
+#include "gds/Input.hpp"
+#include "gds/GUI/fwd.hpp"
+
+#include "../../../_Common/GraphicsTestBase.hpp"
+
+
+class CTestMeshHolder
+{
+public:
+	CMeshObjectHandle m_Handle;
+	Matrix34 m_Pose;
+
+	CMeshResourceDesc m_MeshDesc;
+
+	enum LoadingStyleName
+	{
+		LOAD_SYNCHRONOUSLY,
+		LOAD_MESH_AND_TEX_TOGETHER,
+		LOAD_MESH_AND_TEX_SEPARATELY,
+		NUM_LOADING_STYLES
+	};
+
+	LoadingStyleName m_LoadingStyle;
+
+	CTestMeshHolder( const std::string& filepath, LoadingStyleName loading_style, const Matrix34& pose );
+};
+
+
+class CMultibyteFontTest : public CGraphicsTestBase, public CGraphicsComponent
+{
+	enum Params
+	{
+		TEXT_BUFFER_SIZE = 4096
+	};
+
+//	std::vector<CMeshObjectHandle> m_vecMesh;
+//	std::vector<CTestMeshHolder> m_vecMesh;
+
+	CShaderHandle m_Shader;
+
+	CShaderTechniqueHandle m_MeshTechnique;
+
+	boost::shared_ptr<CInputHandler_Dialog> m_pUIInputHandler;
+
+	boost::shared_ptr<CFontBase> m_pFont;
+
+	CGM_DialogManagerSharedPtr m_pSampleUI;
+
+	CTextureHandle m_TextBufferTexture;
+
+	char m_TextBuffer[TEXT_BUFFER_SIZE];
+
+private:
+
+	void CreateSampleUI();
+
+	bool InitShader();
+
+//	bool CreateFontTextureFromTrueTypeFont( C2DArray<U8>& dest_bitmap_buffer );
+	bool RenderUTF8TextToBufferToImageFile( const std::string& text,
+											 const std::string& font_file,
+											 const int base_char_height
+											  );
+
+public:
+
+	CMultibyteFontTest();
+
+	~CMultibyteFontTest();
+
+	int Init();
+
+	void Release() {};
+
+	void Update( float dt );
+
+	void Render();
+
+	virtual void HandleInput( const SInputData& input );
+
+	void ReleaseGraphicsResources() {}
+	void LoadGraphicsResources( const CGraphicsParameters& rParam ) {}
+};
+
+
+#endif /* __MultibyteFontTest_H__ */
