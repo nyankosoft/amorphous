ce18fe4f7ae1c4bf91f939dabf48445a82f65d44
blue <blue@1b90e699-09eb-4805-8378-d520347fdb25>
2010-07-14 23:27:43 +0000

    added: BitmapImageTest

---
+++ Support/BitmapImageTest/BitmapImageTest.cpp
@@ -0,0 +1,67 @@
+#include "gds/Support/Timer.hpp"
+#include "gds/Support/Profile.hpp"
+#include "gds/Support/BitmapImage.hpp"
+#include "gds/Support/MiscAux.hpp"
+#include "gds/base.hpp"
+
+using namespace std;
+using namespace boost;
+
+
+void LoadImageForTest( const std::string& image_filepath )
+{
+	LOG_SCOPE( image_filepath.c_str() );
+
+	CBitmapImage img;
+	img.LoadFromFile( image_filepath );
+}
+
+
+void TestPerformanceToOpenImageFiles()
+{
+	string img_files[] =
+	{
+		"./images/test51_tex00.bmp",
+		"./images/test51_tex01.bmp",
+		"./images/test51_tex02.bmp",
+		"./images/test51_tex03.bmp",
+		"./images/test51_tex00.jpg",
+		"./images/test51_tex01.jpg",
+		"./images/test51_tex02.jpg",
+		"./images/test51_tex03.jpg"
+	};
+
+	for( int i=0; i<numof(img_files); i++ )
+	{
+		LoadImageForTest( img_files[i] );
+	}
+}
+
+
+int RunTests()
+{
+	CImageArchive img_archive( "./images/src.bmp" );
+
+	if( !img_archive.IsValid() )
+		return -1;
+
+	CBitmapImage img( img_archive );
+
+	bool saved = img.SaveToFile( "./images/dest.jpg" );
+	if( !saved )
+		return -1;
+
+	TestPerformanceToOpenImageFiles();
+
+	return 0;
+}
+
+
+int main( int argc, char *argv[] )
+{
+	string log_filepath = "BitmapImageTest-" + string(GetBuildInfo()) + ".html";
+	shared_ptr<CLogOutput_HTML> pLog( new CLogOutput_HTML(log_filepath) );
+	g_Log.AddLogOutput( pLog.get() );
+
+	return RunTests();
+}
