3fcb3979e3d3a5ac433565219a3cfc17fb0ad108
blue <blue@debad64e-99e9-4aec-818a-2a6ca1ce3416>
2010-09-23 17:50:41 +0000

    updated: scripts

---
+++ Stage/CharacterMotionControl/resources/scripts/empty/collision_tests.py
@@ -0,0 +1,9 @@
+import math3d
+import gfx
+import stage
+import stage_util
+
+misc_util = stage_util.CreateStageMiscUtility()
+
+
+static_mesh = misc_util.CreateStaticTriangleMeshFromMesh( mesh_path="models/character_motion_collision_test.msh" )
