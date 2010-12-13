3a536f50a7fe1ecf71a8e0c85da69c278507fdf3
blue <blue@b80c8c75-5753-0c4f-8f2c-6ef837923b1e>
2010-12-13 00:10:37 +0000

    added: CPhysicsShapeMakerVisitor

---
+++ gds/Utilities/Physics/PhysicsShapeMakerVisitor.hpp
@@ -0,0 +1,81 @@
+#ifndef __PhysicsShapeMakerVisitor_HPP__
+#define __PhysicsShapeMakerVisitor_HPP__
+
+
+#include "../../Graphics/ShapeContainers.hpp"
+#include "../../Graphics/3DGameMath.hpp"
+#include "../../Physics/BoxShapeDesc.hpp"
+#include "../../Physics/SphereShapeDesc.hpp"
+#include "../../Physics/CapsuleShapeDesc.hpp"
+#include "../../Support/Log/DefaultLog.hpp"
+#include "../../Support/SafeDeleteVector.hpp"
+
+
+class CPhysicsShapeMakerVisitor : public CShapeContainerVisitor
+{
+public:
+	std::vector<physics::CShapeDesc *> m_pShapeDescs;
+
+	~CPhysicsShapeMakerVisitor()
+	{
+		SafeDeleteVector( m_pShapeDescs );
+	}
+
+	void VisitAABB3Container( CAABB3Container& aabb3_container )
+	{
+		using namespace physics;
+
+		CBoxShapeDesc *pBox = new CBoxShapeDesc;
+		pBox->vSideLength         = (aabb3_container.aabb.vMax - aabb3_container.aabb.vMin) * 0.5f;
+		pBox->LocalPose.vPosition = aabb3_container.aabb.GetCenterPosition();
+		pBox->LocalPose.matOrient = Matrix33Identity();
+
+		m_pShapeDescs.push_back( pBox );
+	}
+
+	void VisitOBB3Container( COBB3Container& obb3_container )
+	{
+		using namespace physics;
+
+		CBoxShapeDesc *pBox = new CBoxShapeDesc;
+		pBox->vSideLength = obb3_container.obb.radii;
+		pBox->LocalPose   = obb3_container.obb.center;
+
+		m_pShapeDescs.push_back( pBox );
+	}
+
+	void VisitSphereContainer( CSphereContainer& sphere_container )
+	{
+		using namespace physics;
+
+		CSphereShapeDesc *pSphere = new CSphereShapeDesc;
+		pSphere->Radius = sphere_container.sphere.radius;
+		pSphere->LocalPose.vPosition = sphere_container.sphere.center;
+		pSphere->LocalPose.matOrient = Matrix33Identity();
+
+		m_pShapeDescs.push_back( pSphere );
+	}
+
+	void VisitCapsuleContainer( CCapsuleContainer& capsule_container )
+	{
+		using namespace physics;
+
+		CCapsuleShapeDesc *pCap = new CCapsuleShapeDesc;
+		pCap->fRadius = capsule_container.capsule.radius;
+		pCap->fLength = Vec3Length(capsule_container.capsule.p1 - capsule_container.capsule.p0);
+		pCap->LocalPose.vPosition = (capsule_container.capsule.p1 + capsule_container.capsule.p0) * 0.5f;
+		pCap->LocalPose.matOrient = CreateOrientFromFwdDir( Vec3GetNormalized(capsule_container.capsule.p1 - capsule_container.capsule.p0) );
+
+		m_pShapeDescs.push_back( pCap );
+	}
+
+	void VisitConvexContainer( CConvexContainer& convex_container )
+	{ 
+		LOG_PRINT_ERROR( " Not implemented yet." );
+//		m_pShapeDescs.push_back( pConvex );
+	}
+};
+
+
+
+#endif /* __PhysicsShapeMakerVisitor_HPP__ */
