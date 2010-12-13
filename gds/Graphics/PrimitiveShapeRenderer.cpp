f55f4a8336c16909af6e077c9bad4938f0c26e68
blue <blue@b80c8c75-5753-0c4f-8f2c-6ef837923b1e>
2010-12-13 18:39:27 +0000

    added: a draft version of CPrimitiveShapeRenderer

---
+++ gds/Graphics/PrimitiveShapeRenderer.cpp
@@ -0,0 +1,99 @@
+#include "PrimitiveShapeRenderer.hpp"
+#include "Shader/FixedFunctionPipelineManager.hpp"
+#include "Shader/ShaderManager.hpp"
+#include "TextureHandle.hpp"
+#include "3DRect.hpp"
+
+using namespace std;
+
+
+CTextureHandle CreateSingleColorTexture( uint size_x = 1, uint size_y = 1, const SFloatRGBAColor& color = SFloatRGBAColor::White() )
+{
+	CTextureResourceDesc desc;
+	desc.Width     = size_x;
+	desc.Height    = size_y;
+	desc.MipLevels = 0;
+	desc.Format    = TextureFormat::A8R8G8B8;
+	desc.pLoader.reset( new CSignleColorTextureFilling( color ) );
+
+	CTextureHandle tex;
+	bool loaded = tex.Load( desc );
+
+	return tex;
+}
+
+
+
+CPrimitiveShapeRenderer::CPrimitiveShapeRenderer()
+{
+//	CShaderResourceDesc desc;
+//	desc = non_programmable;
+
+//	m_Shader.Load( desc );
+}
+
+void CPrimitiveShapeRenderer::RenderSphere( const Sphere& sphere, const SFloatRGBAColor& color )
+{
+}
+
+
+void CPrimitiveShapeRenderer::RenderBox( const Vector3& vEdgeLengths, const Matrix34& world_pose, const SFloatRGBAColor& color )
+{
+}
+
+
+void CPrimitiveShapeRenderer::RenderCapsule( float radius, float height, const Matrix34& world_pose, const SFloatRGBAColor& color )
+{
+}
+
+
+void CPrimitiveShapeRenderer::RenderCylinder( float radius, float height, const Matrix34& world_pose, const SFloatRGBAColor& color )
+{
+}
+
+
+void CPrimitiveShapeRenderer::RenderAxisAlignedPlane( uint axis, const Vector3& vCenter, float span_0, float span_1, const SFloatRGBAColor& color )
+{
+	if( 6 <= axis )
+		return;
+
+	uint uaxis = axis % 3;
+
+	uint span_index_0 = (uaxis+1) % 3;
+	uint span_index_1 = (uaxis+2) % 3;
+
+	if( 3 <= axis )
+		std::swap( span_index_0, span_index_1 );
+
+	Vector3 normal( Vector3(0,0,0) );
+	normal[uaxis] = (axis < 3) ? 1.0f : -1.0f;
+
+	Vector3 corners[4] =
+	{
+		vCenter,
+		vCenter,
+		vCenter,
+		vCenter
+	};
+	corners[0][span_index_0] += span_0 * 0.5f;
+	corners[0][span_index_1] += span_1 * 0.5f;
+	corners[1][span_index_0] -= span_0 * 0.5f;
+	corners[1][span_index_1] += span_1 * 0.5f;
+	corners[2][span_index_0] -= span_0 * 0.5f;
+	corners[2][span_index_1] -= span_1 * 0.5f;
+	corners[3][span_index_0] += span_0 * 0.5f;
+	corners[3][span_index_1] -= span_1 * 0.5f;
+
+	CTextureHandle s_Texture;
+	if( !s_Texture.IsLoaded() )
+		s_Texture = CreateSingleColorTexture( 1, 1, SFloatRGBAColor::White() );
+
+	FixedFunctionPipelineManager().SetTexture( 0, s_Texture );
+
+	C3DRect rect;
+	rect.SetPositions( corners );
+	rect.SetColor( color );
+	rect.SetNormal( normal );
+
+	rect.Draw();
+}
---
+++ gds/Graphics/PrimitiveShapeRenderer.hpp
@@ -0,0 +1,36 @@
+#ifndef __PrimitiveShapeRenderer_HPP__
+#define __PrimitiveShapeRenderer_HPP__
+
+
+#include "gds/3DMath/Sphere.hpp"
+#include "gds/3DMath/Matrix34.hpp"
+#include "gds/Graphics/ShaderHandle.hpp"
+#include "gds/Graphics/FloatRGBAColor.hpp"
+
+
+class CPrimitiveShapeRenderer
+{
+	CShaderHandle m_Shader; ///< default: fixed function pipeline manager
+
+public:
+
+	CPrimitiveShapeRenderer();
+
+	void RenderSphere( const Sphere& sphere, const SFloatRGBAColor& color = SFloatRGBAColor::White() );
+
+	void RenderBox( const Vector3& vEdgeLengths, const Matrix34& world_pose, const SFloatRGBAColor& color = SFloatRGBAColor::White() );
+
+	void RenderCapsule( float radius, float height, const Matrix34& world_pose, const SFloatRGBAColor& color = SFloatRGBAColor::White() );
+
+	void RenderCylinder( float radius, float height, const Matrix34& world_pose, const SFloatRGBAColor& color = SFloatRGBAColor::White() );
+
+	/// \param axis [in] [0,2] for x, y, and z axis in the positive direction respectively. [3,5] in the negative direction.
+	void RenderAxisAlignedPlane( uint axis = 1, const Vector3& vCenter = Vector3(0,0,0), float span_0 = 100.0f, float span_1 = 100.0f, const SFloatRGBAColor& color = SFloatRGBAColor::White() );
+
+
+	void SetShader( CShaderHandle& shader );
+};
+
+
+
+#endif /* __PrimitiveShapeRenderer_HPP__ */
