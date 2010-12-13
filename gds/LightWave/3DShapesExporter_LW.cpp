682cff2f792c3eb39484480c80d6e3b317974bcc
blue <blue@b80c8c75-5753-0c4f-8f2c-6ef837923b1e>
2010-12-13 00:13:01 +0000

    added: C3DMeshModelBuilder_LW

---
+++ gds/LightWave/3DShapesExporter_LW.cpp
@@ -0,0 +1,44 @@
+#include "3DShapesExporter_LW.hpp"
+#include "3DMeshModelBuilder_LW.hpp"
+#include "LWO2_Object.hpp"
+#include "Graphics/ShapesExtractor.hpp"
+
+using namespace std;
+using namespace boost;
+
+
+bool C3DShapesExporter_LW::ExtractShapes( boost::shared_ptr<CLWO2_Object> pObject, const std::string& output_filepath )
+{
+	if( !pObject )
+		return false;
+
+	// Find layeres whose name begins with "Shapes"
+	SLayerSet layer_set;
+	layer_set.vecpMeshLayer
+		= pObject->GetLayersWithKeyword( "Shapes", CLWO2_NameMatchCond::START_WITH );
+
+	if( layer_set.vecpMeshLayer.empty() )
+	{
+		LOG_PRINT( " Found no layers whose name starts with 'Shapes'" );
+		return false;
+	}
+
+	// Build general 3D mesh(es)
+	boost::shared_ptr<C3DMeshModelBuilder_LW> pLoader( new C3DMeshModelBuilder_LW( pObject ) );
+	pLoader->BuildMeshModel( layer_set );
+
+	shared_ptr<CGeneral3DMesh> pSrcMesh = pLoader->GetGeneral3DMeshSharedPtr();
+	if( !pSrcMesh )
+		return false;
+
+	CShapesExtractor shapes_extractor;
+	Result::Name res = shapes_extractor.ExtractShapes( pSrcMesh, output_filepath );
+
+	return (res == Result::SUCCESS) ? true : false;
+}
+
+
+void C3DShapesExporter_LW::Release()
+{
+	m_pObject.reset();
+}
---
+++ gds/LightWave/3DShapesExporter_LW.hpp
@@ -0,0 +1,40 @@
+#ifndef  __3DShapesExporter_LW_HPP__
+#define  __3DShapesExporter_LW_HPP__
+
+
+#include <string>
+#include <vector>
+#include <boost/shared_ptr.hpp>
+#include "gds/base.hpp"
+#include "gds/Graphics/fwd.hpp"
+#include "gds/LightWave/fwd.hpp"
+
+
+/**
+ * extract shapes from an lwo file
+ * [in] a loaded lwo object or filename of an .lwo files
+ * [out] descriptions of extracted shapes
+ * Steps
+ * - Create a general 3D mesh from the layers named "Shapes" in the input lwo object
+ * - Separates the general 3D mesh into connected sets
+ * - Detect a shape from each connected set
+ */
+class C3DShapesExporter_LW
+{
+
+	boost::shared_ptr<CLWO2_Object> m_pObject;
+
+public:
+
+	C3DShapesExporter_LW() {}
+
+	~C3DShapesExporter_LW() { Release(); }
+
+	void Release();
+
+	bool ExtractShapes( boost::shared_ptr<CLWO2_Object> pObject, const std::string& output_filepath );
+};
+
+
+
+#endif		/*  __3DShapesExporter_LW_HPP__  */
