479caf1fe85e39735ca6ac1698d8f8cb4150fc3d
blue <blue@b80c8c75-5753-0c4f-8f2c-6ef837923b1e>
2011-01-06 01:02:09 +0000

    updated: modified the source code to link to the updated OggVorbis library files. The new versions are static libraries of libogg-1.2.1 and libvorbis-1.3.2.

--- gds/Sound/OggVorbisAux.hpp
+++ gds/Sound/OggVorbisAux.hpp
@@ -11,13 +11,13 @@ class stream_buffer;
 
 
 #ifdef _DEBUG
-	#pragma comment( lib, "ogg_d.lib" )
-	#pragma comment( lib, "vorbis_d.lib" )
-	#pragma comment( lib, "vorbisfile_d.lib" )
+	#pragma comment( lib, "libogg_static_d.lib" )
+	#pragma comment( lib, "libvorbis_static_d.lib" )
+	#pragma comment( lib, "libvorbisfile_static_d.lib" )
 #else
-	#pragma comment( lib, "ogg.lib" )
-	#pragma comment( lib, "vorbis.lib" )
-	#pragma comment( lib, "vorbisfile.lib" )
+	#pragma comment( lib, "libogg_static.lib" )
+	#pragma comment( lib, "libvorbis_static.lib" )
+	#pragma comment( lib, "libvorbisfile_static.lib" )
 #endif
 
 
