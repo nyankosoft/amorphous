7d81ccedb60a0a5497509f70643b2834f6d70003
blue <blue@b80c8c75-5753-0c4f-8f2c-6ef837923b1e>
2011-04-03 00:28:51 +0000

    moved: Direct3D-dependent code to Graphics/Direct3D

--- gds/Graphics/AlphaBlend.hpp
+++ gds/Graphics/AlphaBlend.hpp
@@ -1,6 +1,7 @@
 #ifndef __ALPHABLEND_H__
 #define __ALPHABLEND_H__
 
+
 class AlphaBlend
 {
 public:
@@ -33,42 +34,6 @@ public:
 	};
 };
 
-//---------------------------- D3D adapter ----------------------------
-
-#include <d3dx9.h>
-
-// conversion table: generic alpha blend mode enums > d3d blend enums
-static const DWORD g_dwD3DBlendMode[] =
-{
-	D3DBLEND_ZERO,
-    D3DBLEND_ONE,
-    D3DBLEND_SRCALPHA,
-    D3DBLEND_INVSRCALPHA,
-    D3DBLEND_DESTALPHA,
-    D3DBLEND_INVDESTALPHA,
-	D3DBLEND_DESTCOLOR,
-	D3DBLEND_INVDESTCOLOR,
-	D3DBLEND_SRCALPHASAT
-};
-
-
-static const DWORD g_dwD3DTextureAddressMode[] =
-{
-	D3DTADDRESS_WRAP,
-	D3DTADDRESS_MIRROR,
-	D3DTADDRESS_CLAMP,
-	D3DTADDRESS_BORDER,
-	D3DTADDRESS_MIRRORONCE
-};
-
-
-inline DWORD ToD3DTextureAddressMode( TextureAddress::Mode address_mode )
-{
-	return g_dwD3DTextureAddressMode[ (int)address_mode ];
-}
-
-
-//---------------------------- D3D adapter ----------------------------
 
 
 #endif  /* __ALPHABLEND_H__ */
---
+++ gds/Graphics/Direct3D/D3DAlphaBlend.hpp
@@ -0,0 +1,44 @@
+#ifndef __D3DAlphaBlend_HPP__
+#define __D3DAlphaBlend_HPP__
+
+
+#include <d3dx9.h>
+#include "../AlphaBlend.hpp"
+
+
+// conversion table: generic alpha blend mode enums > d3d blend enums
+static const DWORD g_dwD3DBlendMode[] =
+{
+	D3DBLEND_ZERO,
+    D3DBLEND_ONE,
+    D3DBLEND_SRCALPHA,
+    D3DBLEND_INVSRCALPHA,
+    D3DBLEND_DESTALPHA,
+    D3DBLEND_INVDESTALPHA,
+	D3DBLEND_DESTCOLOR,
+	D3DBLEND_INVDESTCOLOR,
+	D3DBLEND_SRCALPHASAT
+};
+
+
+static const DWORD g_dwD3DTextureAddressMode[] =
+{
+	D3DTADDRESS_WRAP,
+	D3DTADDRESS_MIRROR,
+	D3DTADDRESS_CLAMP,
+	D3DTADDRESS_BORDER,
+	D3DTADDRESS_MIRRORONCE
+};
+
+
+inline DWORD ToD3DTextureAddressMode( TextureAddress::Mode address_mode )
+{
+	return g_dwD3DTextureAddressMode[ (int)address_mode ];
+}
+
+
+//---------------------------- D3D adapter ----------------------------
+
+
+
+#endif /* __D3DAlphaBlend_HPP__ */
