319dc173861fdcca44ac69913b5175776c1db8f9
blue <blue@b80c8c75-5753-0c4f-8f2c-6ef837923b1e>
2012-03-04 14:05:21 +0000

    added: Set6FaceColors() - Used to set the face colors of a box custom mesh.

--- gds/Graphics/MeshUtilities.hpp
+++ gds/Graphics/MeshUtilities.hpp
@@ -4,6 +4,7 @@
 
 #include "MeshGenerators.hpp"
 #include "MeshObjectHandle.hpp"
+#include "Mesh/CustomMesh.hpp"
 
 
 /// \param whd [in] width(x), height(y), and depth of the box
@@ -77,6 +78,37 @@ inline CMeshObjectHandle CreateConeMesh( float radius = 0.5f, float height = 1.0
 	desc.cone_height = height;
 
 	return CreateConeMesh( desc, diffuse_color, model_pose );
+}
+
+
+inline Result::Name Set6FaceColors(
+	CCustomMesh& box_mesh,
+	const SFloatRGBAColor& px = SFloatRGBAColor::Red(),
+	const SFloatRGBAColor& py = SFloatRGBAColor::Green(),
+	const SFloatRGBAColor& pz = SFloatRGBAColor::Blue(),
+	const SFloatRGBAColor& nx = SFloatRGBAColor::Aqua(),
+	const SFloatRGBAColor& ny = SFloatRGBAColor::Magenta(),
+	const SFloatRGBAColor& nz = SFloatRGBAColor::Yellow()
+	)
+{
+	SFloatRGBAColor face_colors[6] = { py, ny, nz, pz, px, nx };
+
+	if( box_mesh.GetNumVertices() != 24 )
+		return Result::INVALID_ARGS;
+
+	std::vector<SFloatRGBAColor> diffuse_colors;
+	diffuse_colors.resize( 24 );
+	for( int i=0; i<6; i++ )
+	{
+		for( int j=0; j<4; j++ )
+		{
+			diffuse_colors[ i * 4 + j ] = face_colors[i];
+		}
+	}
+
+	box_mesh.SetDiffuseColors( diffuse_colors );
+
+	return Result::SUCCESS;
 }
 
 
