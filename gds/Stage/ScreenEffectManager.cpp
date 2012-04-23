6dea309e4169bb5714560221cf8d0a5983ae63e6
blue <blue@b80c8c75-5753-0c4f-8f2c-6ef837923b1e>
2012-04-23 22:23:16 +0000

    cleaned up: the source code

--- gds/Stage/ScreenEffectManager.cpp
+++ gds/Stage/ScreenEffectManager.cpp
@@ -1,9 +1,7 @@
 #include "ScreenEffectManager.hpp"
 #include <algorithm>
 #include "3DMath/MathMisc.hpp"
-//#include "Graphics/Direct3D/Direct3D9.hpp"
 #include "Graphics/Camera.hpp"
-//#include "Graphics/Shader/Shader.hpp"
 #include "Graphics/Shader/ShaderManager.hpp"
 #include "Graphics/PostProcessEffectManager.hpp"
 #include "Graphics/SimpleMotionBlur.hpp"
