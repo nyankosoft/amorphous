03302d9023f95bf5a30d951ce69e7c07c1b382c4
blue <blue@b80c8c75-5753-0c4f-8f2c-6ef837923b1e>
2012-09-27 18:47:36 +0000

    split: files for CMeshGenerator and its derived classes.

---
+++ gds/Graphics/MeshGenerator.hpp
@@ -0,0 +1,81 @@
+#ifndef __MeshGenerator_HPP__
+#define __MeshGenerator_HPP__
+
+
+#include "../base.hpp"
+#include "MeshModel/PrimitiveShapeMeshes.hpp"
+#include "MeshModel/3DMeshModelArchive.hpp"
+using namespace MeshModel;
+
+
+class CMeshGenerator
+{
+protected:
+
+	C3DMeshModelArchive m_MeshArchive;
+
+	SFloatRGBAColor m_DiffuseColor;
+
+	U32 m_RequestedVertexFormatFlags;
+
+	std::string m_TexturePath;
+
+	std::string m_ResourceIDString;
+
+	U32 m_TexCoordStyleFlags;
+
+	MeshPolygonDirection::Type m_PolygonDirection;
+
+protected:
+
+	/// Generate() of subclasses need to call this after creating mesh
+	/// and store it to m_MeshArchive
+	void SetMiscMeshAttributes();
+
+	/// Derived classes are responsible for calling this function if it wants to
+	/// generate texture coords based on m_TexCoordStyleFlags.
+	void GenerateTextureCoords( CGeneral3DMesh& mesh );
+
+	Result::Name CreateArchiveFromGeneral3DMesh( boost::shared_ptr<CGeneral3DMesh>& pSrcMesh );
+
+public:
+
+	enum Params
+	{
+		DEFAULT_VERTEX_FLAGS
+		= CMMA_VertexSet::VF_POSITION
+		|CMMA_VertexSet::VF_NORMAL
+		|CMMA_VertexSet::VF_DIFFUSE_COLOR
+		|CMMA_VertexSet::VF_2D_TEXCOORD0
+	};
+
+	CMeshGenerator()
+		:
+	m_DiffuseColor( SFloatRGBAColor(1.0f,1.0f,1.0f,1.0f) ),
+	m_RequestedVertexFormatFlags( DEFAULT_VERTEX_FLAGS ),
+	m_TexCoordStyleFlags(0),
+	m_PolygonDirection(MeshPolygonDirection::OUTWARD)
+	{}
+
+	virtual ~CMeshGenerator() {}
+
+	const C3DMeshModelArchive& GetMeshArchive() const { return m_MeshArchive; }
+
+	C3DMeshModelArchive& MeshArchive() { return m_MeshArchive; }
+
+	void SetVertexFormatFlags( U32 vert_flags ) { m_RequestedVertexFormatFlags = vert_flags; }
+
+	void SetDiffuseColor( const SFloatRGBAColor &diffuse_color ) { m_DiffuseColor = diffuse_color; }
+
+	void SetTexturePath( const std::string& texture_path ) { m_TexturePath = texture_path; }
+
+	void SetTexCoordStyleFlags( U32 flags ) { m_TexCoordStyleFlags = flags; }
+
+	void SetPolygonDirection( MeshPolygonDirection::Type polygon_direction ) { m_PolygonDirection = polygon_direction; }
+
+	virtual Result::Name Generate() = 0;
+};
+
+
+
+#endif /* __MeshGenerator_HPP__ */
--- gds/Graphics/MeshGenerators.hpp
+++ gds/Graphics/MeshGenerators.hpp
@@ -1,11 +1,8 @@
-#ifndef __MeshGenerator_H__
-#define __MeshGenerator_H__
+#ifndef __MeshGenerators_HPP__
+#define __MeshGenerators_HPP__
 
 
-#include "../base.hpp"
-#include "MeshModel/PrimitiveShapeMeshes.hpp"
-#include "MeshModel/3DMeshModelArchive.hpp"
-using namespace MeshModel;
+#include "MeshGenerator.hpp"
 
 
 class TexCoordStyle
@@ -20,75 +17,6 @@ public:
 //		LINEAR_SHIFT_Z     = (1 << 4),
 //		LINEAR_SHIFT_INV_Z = (1 << 5),
 	};
-};
-
-
-class CMeshGenerator
-{
-protected:
-
-	C3DMeshModelArchive m_MeshArchive;
-
-	SFloatRGBAColor m_DiffuseColor;
-
-	U32 m_RequestedVertexFormatFlags;
-
-	std::string m_TexturePath;
-
-	std::string m_ResourceIDString;
-
-	U32 m_TexCoordStyleFlags;
-
-	MeshPolygonDirection::Type m_PolygonDirection;
-
-protected:
-
-	/// Generate() of subclasses need to call this after creating mesh
-	/// and store it to m_MeshArchive
-	void SetMiscMeshAttributes();
-
-	/// Derived classes are responsible for calling this function if it wants to
-	/// generate texture coords based on m_TexCoordStyleFlags.
-	void GenerateTextureCoords( CGeneral3DMesh& mesh );
-
-	Result::Name CreateArchiveFromGeneral3DMesh( boost::shared_ptr<CGeneral3DMesh>& pSrcMesh );
-
-public:
-
-	enum Params
-	{
-		DEFAULT_VERTEX_FLAGS
-		= CMMA_VertexSet::VF_POSITION
-		|CMMA_VertexSet::VF_NORMAL
-		|CMMA_VertexSet::VF_DIFFUSE_COLOR
-		|CMMA_VertexSet::VF_2D_TEXCOORD0
-	};
-
-	CMeshGenerator()
-		:
-	m_DiffuseColor( SFloatRGBAColor(1.0f,1.0f,1.0f,1.0f) ),
-	m_RequestedVertexFormatFlags( DEFAULT_VERTEX_FLAGS ),
-	m_TexCoordStyleFlags(0),
-	m_PolygonDirection(MeshPolygonDirection::OUTWARD)
-	{}
-
-	virtual ~CMeshGenerator() {}
-
-	const C3DMeshModelArchive& GetMeshArchive() const { return m_MeshArchive; }
-
-	C3DMeshModelArchive& MeshArchive() { return m_MeshArchive; }
-
-	void SetVertexFormatFlags( U32 vert_flags ) { m_RequestedVertexFormatFlags = vert_flags; }
-
-	void SetDiffuseColor( const SFloatRGBAColor &diffuse_color ) { m_DiffuseColor = diffuse_color; }
-
-	void SetTexturePath( const std::string& texture_path ) { m_TexturePath = texture_path; }
-
-	void SetTexCoordStyleFlags( U32 flags ) { m_TexCoordStyleFlags = flags; }
-
-	void SetPolygonDirection( MeshPolygonDirection::Type polygon_direction ) { m_PolygonDirection = polygon_direction; }
-
-	virtual Result::Name Generate() = 0;
 };
 
 
@@ -176,4 +104,4 @@ public:
 };
 
 
-#endif  /* __MeshGenerator_H__ */
+#endif  /* __MeshGenerators_HPP__ */
