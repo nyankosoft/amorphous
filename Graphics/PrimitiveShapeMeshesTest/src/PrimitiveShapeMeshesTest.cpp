da174cb3aa95964f7197d6992784f7911febfe98
blue <blue@1b90e699-09eb-4805-8378-d520347fdb25>
2012-10-21 17:45:26 +0000

    CPrimitiveShapeMeshesTest::Render() - Stopped accessing a global variable.

--- Graphics/PrimitiveShapeMeshesTest/src/PrimitiveShapeMeshesTest.cpp
+++ Graphics/PrimitiveShapeMeshesTest/src/PrimitiveShapeMeshesTest.cpp
@@ -221,7 +221,7 @@ void CPrimitiveShapeMeshesTest::Render()
 */
 	m_pFont->DrawText( m_TextBuffer, vTopLeft );
 
-	Vector3 vCamPos = g_Camera.GetPosition();
+	Vector3 vCamPos = GetCurrentCamera().GetPosition();
 	m_pFont->DrawText(
 		fmt_string( "x: %f\ny: %f\nz: %f\n", vCamPos.x, vCamPos.y, vCamPos.z ),
 		Vector2( 20, 300 ) );
