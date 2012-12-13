ab7121ec16b064a5cb034a5fc9817a95bea45d63
blue <blue@b80c8c75-5753-0c4f-8f2c-6ef837923b1e>
2012-12-13 16:42:39 +0000

    changed: CreateUniformCylindricalHelix() - Converted to a template function so that it can be used for both float and double.

--- gds/3DMath/HelixPolygonModelMaker.cpp
+++ gds/3DMath/HelixPolygonModelMaker.cpp
@@ -1,4 +1,5 @@
 #include "HelixPolygonModelMaker.hpp"
+#include "Matrix33.hpp"
 
 
 static void CreateHelix_AddCordFacePolygon(
@@ -24,19 +25,20 @@ static void CreateHelix_AddCordFacePolygon(
 }
 
 
+template<typename T>
 void CreateUniformCylindricalHelix(
-	float helix_length,
-	float helix_radius,
+	T helix_length,
+	T helix_radius,
 	unsigned int num_coils,
-	float cord_radius,
+	T cord_radius,
 	unsigned int num_helix_sides,
 	unsigned int num_cord_sides,
 	bool create_top_face,
 	PolygonModelStyle::Name top_face_style,
 	bool create_bottom_face,
 	PolygonModelStyle::Name bottom_face_style,
-	std::vector<Vector3>& positions,
-	std::vector<Vector3>& normals,
+	std::vector< tVector3<T> >& positions,
+	std::vector< tVector3<T> >& normals,
 	std::vector< std::vector<unsigned int> >& polygons
 	)
 {
@@ -49,35 +51,35 @@ void CreateUniformCylindricalHelix(
 
 	unsigned int vertex_index_offset = (unsigned int)positions.size();
 
-	const float single_helix_length = helix_length / (float)num_coils;
-	const float tilt_angle = atan( single_helix_length / (2.0f * (float)PI * helix_radius) );
-	const Matrix33 face_tilt( Matrix33RotationZ( tilt_angle ) );
+	const T single_helix_length = helix_length / (T)num_coils;
+	const T tilt_angle = atan( single_helix_length / ((T)2.0 * (T)PI * helix_radius) );
+	const tMatrix33<T> face_tilt( Matrix33RotationZ( tilt_angle ) );
 
-	std::vector<Vector3> face_vertices;
+	std::vector< tVector3<T> > face_vertices;
 	face_vertices.resize( num_cord_sides );
 	for( unsigned int i=0; i<num_cord_sides; i++ )
 	{
-		float angle = (float)i / (float)num_cord_sides * 2.0f * (float)PI;
-		face_vertices[i] = Vector3( cos(angle), sin(angle), 0.0f ) * cord_radius;
+		T angle = (T)i / (T)num_cord_sides * (T)2.0 * (T)PI;
+		face_vertices[i] = tVector3<T>( cos(angle), sin(angle), 0.0f ) * cord_radius;
 		face_vertices[i].x += (helix_radius - cord_radius);
 
 		face_vertices[i] = face_tilt * face_vertices[i];
 	}
 
-	std::vector<Vector3> single_helix_face_vertices;
+	std::vector< tVector3<T> > single_helix_face_vertices;
 	single_helix_face_vertices.resize( num_cord_sides * num_helix_sides );
 
 	// Create vertices of the single coil
-	float step_height = single_helix_length / (float)num_helix_sides;
+	T step_height = single_helix_length / (T)num_helix_sides;
 	for( unsigned int i=0; i<num_helix_sides; i++ )
 	{
-		float heading_angle = (float)i / (float)num_helix_sides * 2.0f * (float)PI;
-		Matrix33 face_heading( Matrix33RotationY( heading_angle ) );
-		float height_offset = step_height * (float)i;
+		T heading_angle = (T)i / (T)num_helix_sides * (T)2.0 * (T)PI;
+		tMatrix33<T> face_heading( Matrix33RotationY( heading_angle ) );
+		T height_offset = step_height * (T)i;
 
 		for( unsigned int j=0; j<num_cord_sides; j++ )
 		{
-			Vector3& pos = single_helix_face_vertices[num_cord_sides*i+j];
+			tVector3<T>& pos = single_helix_face_vertices[num_cord_sides*i+j];
 			pos = face_heading * face_vertices[j];
 			pos.y += height_offset;
 		}
@@ -88,7 +90,7 @@ void CreateUniformCylindricalHelix(
 	for( unsigned int i=0; i<num_coils; i++ )
 	{
 		int helix_start_offset = (int)positions.size();
-		float helix_start_height = single_helix_length * (float)i;
+		T helix_start_height = single_helix_length * (T)i;
 
 		positions.insert(
 			positions.end(),
@@ -159,3 +161,36 @@ void CreateUniformCylindricalHelix(
 	}
 }
 
+
+// Explicit instantiations
+template void CreateUniformCylindricalHelix(
+	float helix_length,
+	float helix_radius,
+	unsigned int num_coils,
+	float cord_radius,
+	unsigned int num_helix_sides,
+	unsigned int num_cord_sides,
+	bool create_top_face,
+	PolygonModelStyle::Name top_face_style,
+	bool create_bottom_face,
+	PolygonModelStyle::Name bottom_face_style,
+	std::vector<Vector3>& positions,
+	std::vector<Vector3>& normals,
+	std::vector< std::vector<unsigned int> >& polygons
+	);
+
+template void CreateUniformCylindricalHelix(
+	double helix_length,
+	double helix_radius,
+	unsigned int num_coils,
+	double cord_radius,
+	unsigned int num_helix_sides,
+	unsigned int num_cord_sides,
+	bool create_top_face,
+	PolygonModelStyle::Name top_face_style,
+	bool create_bottom_face,
+	PolygonModelStyle::Name bottom_face_style,
+	std::vector< tVector3<double> >& positions,
+	std::vector< tVector3<double> >& normals,
+	std::vector< std::vector<unsigned int> >& polygons
+	);
--- gds/3DMath/HelixPolygonModelMaker.hpp
+++ gds/3DMath/HelixPolygonModelMaker.hpp
@@ -3,23 +3,24 @@
 
 
 #include <vector>
-#include "Matrix33.hpp"
 #include "PolygonModelStyle.hpp"
+#include "Vector3.hpp"
 
 
+template<typename T>
 void CreateUniformCylindricalHelix(
-	float helix_length,
-	float helix_radius,
+	T helix_length,
+	T helix_radius,
 	unsigned int num_coils,
-	float cord_radius,
+	T cord_radius,
 	unsigned int num_helix_sides,
 	unsigned int num_cord_sides,
 	bool create_top_face,
 	PolygonModelStyle::Name top_face_style,
 	bool create_bottom_face,
 	PolygonModelStyle::Name bottom_face_style,
-	std::vector<Vector3>& positions,
-	std::vector<Vector3>& normals,
+	std::vector< tVector3<T> >& positions,
+	std::vector< tVector3<T> >& normals,
 	std::vector< std::vector<unsigned int> >& polygons
 	);
 
