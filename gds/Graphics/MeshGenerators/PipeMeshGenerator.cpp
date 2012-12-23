38abeb5de6b810120ebd9a46a86a54fde1009dfa
blue <blue@b80c8c75-5753-0c4f-8f2c-6ef837923b1e>
2012-12-23 11:02:45 +0000

    added: PipeMeshGenerator - Creates a cylindrical mesh with a hole along the axis.

---
+++ gds/Graphics/MeshGenerators/PipeMeshGenerator.cpp
@@ -0,0 +1,55 @@
+#include "PipeMeshGenerator.hpp"
+#include "../../3DMath/PrimitivePolygonModelMaker.hpp"
+#include "../MeshModel/General3DMesh.hpp"
+
+using std::vector;
+using boost::shared_ptr;
+
+
+Result::Name PipeMeshGenerator::Generate()
+{
+	vector<Vector3> positions, normals;
+	vector< vector<int> > polygons;
+
+	float length = m_Desc.height;
+	float outer_radius = m_Desc.radii[0];
+	float inner_radius = outer_radius - m_fWallThickness;
+	CreatePipe(
+		length, outer_radius, inner_radius,
+		m_Desc.num_sides,
+		m_Desc.num_divisions,
+		true,
+		PolygonModelStyle::EDGE_VERTICES_UNWELDED,
+		true,
+		PolygonModelStyle::EDGE_VERTICES_UNWELDED,
+		positions, normals, polygons
+		);
+
+	// Copy vertices and polygons to a general 3D mesh.
+
+	shared_ptr<CGeneral3DMesh> pMesh( new CGeneral3DMesh() );
+
+	CGeneral3DMesh& mesh = *pMesh;
+
+	mesh.SetVertexFormatFlags(
+		 CMMA_VertexSet::VF_POSITION
+		|CMMA_VertexSet::VF_NORMAL
+		|CMMA_VertexSet::VF_DIFFUSE_COLOR
+		|CMMA_VertexSet::VF_2D_TEXCOORD0 );
+
+	vector<TEXCOORD2> tex_uvs;
+	tex_uvs.resize( positions.size(), TEXCOORD2(0,0) );
+
+	mesh.SetVertices( positions, normals, tex_uvs );
+
+	mesh.SetPolygons( polygons );
+
+	mesh.GetMaterialBuffer().resize( 1 );
+
+	Result::Name res = ::CreateArchiveFromGeneral3DMesh( pMesh, m_MeshArchive );
+
+	SetMiscMeshAttributes(); // set texture filename if it is specified.
+
+	return res;
+}
+
---
+++ gds/Graphics/MeshGenerators/PipeMeshGenerator.hpp
@@ -0,0 +1,43 @@
+#ifndef __PipeMeshGenerator_HPP__
+#define __PipeMeshGenerator_HPP__
+
+
+#include "MeshGenerators.hpp"
+
+
+class PipeMeshGenerator : public CMeshGenerator
+{
+	CCylinderDesc m_Desc;
+
+	float m_fWallThickness;
+
+	Result::Name CreateVertices( CGeneral3DMesh& mesh );
+
+	Result::Name CreatePolygons( CGeneral3DMesh& mesh );
+
+public:
+
+	PipeMeshGenerator( float length = 1.00f, float outer_radius = 0.25f, float inner_radius = 0.20f, unsigned int num_sides = 32, unsigned int num_divisions = 1 )
+	{
+		m_Desc.height        = length;
+		m_Desc.radii[0]      = m_Desc.radii[1] = outer_radius;
+		m_Desc.num_sides     = num_sides;
+		m_Desc.num_divisions = num_divisions;
+		m_fWallThickness     = outer_radius - inner_radius;
+	}
+
+	PipeMeshGenerator( const CCylinderDesc& desc, float wall_thickness )
+		:
+	m_Desc(desc),
+	m_fWallThickness(wall_thickness)
+	{}
+
+	Result::Name Generate();
+};
+
+
+//CMeshObjectHandle CreatePipeMesh( float outer_radius, float inner_radius, float height, unsigned int num_sides );
+
+
+
+#endif /* __PipeMeshGenerator_HPP__ */
