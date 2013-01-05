fcba6b023d474c0d856064dc52d21317008749df
blue <blue@b80c8c75-5753-0c4f-8f2c-6ef837923b1e>
2013-01-05 02:16:23 +0000

    added: a new class, 'indexed_vector' - std::vector + an index

---
+++ gds/Support/indexed_vector.hpp
@@ -0,0 +1,102 @@
+#ifndef __indexed_vector_HPP__
+#define __indexed_vector_HPP__
+
+
+#include <vector>
+
+
+namespace amorphous
+{
+
+
+template<typename T>
+class indexed_vector
+{
+	std::vector<T> m_elements;
+
+	unsigned int m_index;
+
+public:
+
+	indexed_vector() : m_index(0) {}
+
+	bool get_current( T& dest )
+	{
+		if( m_elements.empty() )
+			return false;
+
+		if( (unsigned int)m_elements.size() <= m_index )
+			return false;
+
+		dest = m_elements[m_index];
+		return true;
+	}
+
+	void operator++()
+	{
+		if( m_elements.empty() )
+			return;
+
+		if( m_index == (unsigned int)m_elements.size() -1 )
+			m_index = 0;
+		else
+			m_index += 1;
+	}
+
+	void operator--()
+	{
+		if( m_elements.empty() )
+			return;
+
+		// Of course, you can write it with a single line,
+//		m_index = (m_index + m_elements.size() - 1) % m_elements.size();
+
+		// but we just do it in a more intuitive way.
+		if( m_index == 0 )
+			m_index = (unsigned int)m_elements.size() - 1;
+		else
+			m_index -= 1;
+	}
+
+	void push_back( const T& val )
+	{
+		m_elements.push_back( val );
+	}
+
+	void resize( unsigned int size )
+	{
+		m_elements.resize( size );
+	}
+
+	bool empty() const
+	{
+		return m_elements.empty();
+	}
+
+	void clear()
+	{
+		m_elements.clear();
+	}
+
+	const T& operator[]( unsigned int index ) const
+	{
+		return m_elements[index];
+	}
+
+	T& operator[]( unsigned int index )
+	{
+		return m_elements[index];
+	}
+
+	void operator=( const std::vector<T>& src )
+	{
+		m_elements = src;
+	}
+};
+
+
+} // amorphous
+
+
+
+#endif /* __indexed_vector_HPP__ */
