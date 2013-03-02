660c4665f12e589a0e655f23ea2ac2dc089b9906
blue <blue@b80c8c75-5753-0c4f-8f2c-6ef837923b1e>
2013-03-02 15:25:04 +0000

    added: PlayerCustomer

---
+++ gds/GameCommon/PlayerCustomer.hpp
@@ -0,0 +1,47 @@
+#ifndef __PlayerCustomer_HPP__
+#define __PlayerCustomer_HPP__
+
+
+#include "GameItemShop.hpp"
+#include "../Stage/PlayerInfo.hpp"
+
+
+namespace amorphous
+{
+
+
+class PlayerCustomer : public CCustomer
+{
+public:
+
+	virtual int AddItem( boost::shared_ptr<GameItem> pItem )
+	{
+		if( !pItem )
+			return 0;
+
+//		GameItem* pTgtItem = SinglePlayerInfo().GetItemByName( pItem->GetName() );
+//		int orig_quantity = 0;
+//		if( pTgtItem ) orig_quantity = pTgtItem->GetCurrentQuantity();
+
+		return SinglePlayerInfo().SupplyItem( pItem );
+	}
+
+	virtual bool Pay( int amount )
+	{
+		if( SinglePlayerInfo().m_Money < amount )
+			return false;
+
+		SinglePlayerInfo().m_Money -= amount;
+		return true;
+	}
+
+	virtual int GetMoneyLeft() const { return SinglePlayerInfo().m_Money; }
+
+	virtual const GameItem& GetItem( const std::string& name ) { return *SinglePlayerInfo().GetItemByName<GameItem>( name.c_str() ).get(); }
+};
+
+
+} // namespace amorphous
+
+
+#endif /* __PlayerCustomer_HPP__ */
