2a08e82cc10bc99a6f6e02a641b363207e55974e
blue <blue@b80c8c75-5753-0c4f-8f2c-6ef837923b1e>
2013-03-16 01:20:58 +0000

    Updated: the comments. - Translated the comments from Japanese to English in order to save the file in ASCII format.

--- gds/Support/WindowMisc_Win32.hpp
+++ gds/Support/WindowMisc_Win32.hpp
@@ -9,22 +9,23 @@ namespace amorphous
 {
 
 
+/// Returns the horizontal and vertical dimensions which do not belong to the client area in the window.
 inline void GetNonClientAreaSize( HWND hWnd, long& frame_width, long& frame_height )
 {
 	RECT rect;
 	int ww, wh, cw, ch;
 
 	// calc boundary width outside the client rect
-	GetClientRect(hWnd, &rect);		// クライアント部分のサイズの取得
-	cw = rect.right - rect.left;	// クライアント領域外の横幅を計算
-	ch = rect.bottom - rect.top;	// クライアント領域外の縦幅を計算
+	GetClientRect(hWnd, &rect);		// Get the size of the client area
+	cw = rect.right - rect.left;	// The client area width
+	ch = rect.bottom - rect.top;	// The client area width
 
 	// calc the entire window size
-	GetWindowRect(hWnd, &rect);		// ウインドウ全体のサイズ取得
-	ww = rect.right - rect.left;	// ウインドウ全体の幅の横幅を計算
-	wh = rect.bottom - rect.top;	// ウインドウ全体の幅の縦幅を計算
-	frame_width  = ww - cw;			// クライアント領域以外に必要な幅
-	frame_height = wh - ch;			// クライアント領域以外に必要な高さ
+	GetWindowRect(hWnd, &rect);		// Get the size of the entire window
+	ww = rect.right - rect.left;	// The window width
+	wh = rect.bottom - rect.top;	// The window height
+	frame_width  = ww - cw;			// The horizontal dimension which does not belong to the client area in the window
+	frame_height = wh - ch;			// The vertical dimension which does not belong to the client area in the window
 }
 
 
