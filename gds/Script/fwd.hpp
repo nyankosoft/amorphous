880e74ae9ae950022fc3076f12988295a8344ca4
blue <blue@b80c8c75-5753-0c4f-8f2c-6ef837923b1e>
2013-03-24 17:11:56 +0000

    added: the "amorphous" namesapce and some more forward declarations of classes

--- gds/Script/fwd.hpp
+++ gds/Script/fwd.hpp
@@ -2,7 +2,17 @@
 #define __FWD_Script_HPP__
 
 
+namespace amorphous
+{
+
+
 class CEmbeddedPythonModule;
+class ScriptManager;
+class PythonScriptManager;
+class BoostPythonScriptManager;
+
+
+} // namespace amorphous
 
 
 #endif /* __FWD_Script_HPP__ */
