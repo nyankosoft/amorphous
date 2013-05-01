333afcada0b377cbe6bd8ec5873363fce87824f9
blue <blue@b80c8c75-5753-0c4f-8f2c-6ef837923b1e>
2013-05-01 10:30:11 +0000

    cleaned up: the source code

--- amorphous/Graphics/OpenGL/Mesh/GLProgressiveMeshImpl.hpp
+++ amorphous/Graphics/OpenGL/Mesh/GLProgressiveMeshImpl.hpp
@@ -22,26 +22,6 @@ public:
 	virtual ~CGLProgressiveMeshImpl();
 
 	void Release() {}
-
-//	virtual bool LoadFromArchive( C3DMeshModelArchive& archive, const std::string& filename, U32 option_flags );
-
-//	bool LoadFromFile( const std::string& filename, U32 option_flags = 0 );
-
-//	virtual bool CreateMesh( int num_vertices, int num_indices, U32 option_flags, std::vector<D3DVERTEXELEMENT9>& vecVertexElement ) {}
-
-	/// render object by using the fixed function pipeline
-//	void Render();
-
-//	virtual void Render( ShaderManager& rShaderMgr );
-
-//	virtual void RenderSubsets( ShaderManager& rShaderMgr,
-//		                        const std::vector<int>& vecMaterialIndex,
-//								std::vector<ShaderTechniqueHandle>& vecShaderTechnique );
-
-//	virtual void RenderSubsets( ShaderManager& rShaderMgr,
-//		                        const std::vector<int>& vecMaterialIndex /* some option to specify handles for texture */);
-
-//	virtual MeshType::Name GetMeshType() const { return MeshType::PROGRESSIVE; }
 };
 
 
