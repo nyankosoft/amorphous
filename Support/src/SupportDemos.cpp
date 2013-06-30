4375a83612b4826c43b054dd1e12038a0b1d28a0
blue <sgt.nyanko@gmail.com>
2013-06-30 22:08:15 +0900

    added: the source file to run different demos.

---
+++ Support/src/SupportDemos.cpp
@@ -0,0 +1,12 @@
+
+
+
+int test_fixed_string( int argc, char *argv[] );;
+int test_clipboard_utils( int argc, char *argv[] );
+int test_BitmapImage( int argc, char *argv[] );
+
+
+int main( int argc, char *argv[] )
+{
+	return 0;
+}
