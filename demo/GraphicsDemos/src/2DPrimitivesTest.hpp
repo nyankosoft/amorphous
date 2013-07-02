34fb0e801f1707898be24115792b40e2d79ce637
blue <sgt.nyanko@gmail.com>
2013-07-02 23:55:01 +0900

    updated: C2DPrimitivesTest

--- demo/GraphicsDemos/src/2DPrimitivesTest.hpp
+++ demo/GraphicsDemos/src/2DPrimitivesTest.hpp
@@ -2,8 +2,10 @@
 #define  __2DPrimitivesTest_HPP__
 
 
+#include "amorphous/Graphics/2DPrimitive/2DRect.hpp"
 #include "amorphous/Graphics/2DPrimitive/2DFrameRect.hpp"
 #include "amorphous/Graphics/2DPrimitive/2DRoundRect.hpp"
+#include "amorphous/Graphics/ShaderHandle.hpp"
 #include "amorphous/Input/fwd.hpp"
 
 #include "../../_Common/GraphicsTestBase.hpp"
@@ -11,20 +13,36 @@
 
 class C2DPrimitivesTest : public CGraphicsTestBase
 {
-//	boost::shared_ptr<FontBase> m_pFont;
-
 	enum Params
 	{
-		NUM_2D_PRIMITIVE_TYPES_TO_RENDER = 2,
+		NUM_2D_PRIMITIVE_TYPES_TO_RENDER = 4,
 	};
 
 	int m_Type;
 
-	std::vector<TextureHandle> m_vecTexture;
+	std::vector<TextureHandle> m_RectTextures;
+
+	std::vector<TextureHandle> m_FrameTextures;
+
+	std::vector<C2DRect> m_Rects;
+
+	std::vector<C2DRoundRect> m_RoundRects;
 
 	std::vector<C2DFrameRect> m_FrameRects;
 
 	std::vector<C2DRoundFrameRect> m_RoundFrameRects;
+
+	ShaderHandle m_Shader;
+
+	bool m_UseTexture;
+
+private:
+
+	int InitRects();
+
+	int InitFrameRects();
+
+	void RenderRects();
 
 public:
 
