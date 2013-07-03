33995e9292cb535ef3e53eee496c99ac0096faa4
blue <sgt.nyanko@gmail.com>
2013-07-03 23:11:19 +0900

    added: CPrecipitationEffectTest

---
+++ demo/GraphicsDemos/src/PrecipitationEffectTest.hpp
@@ -0,0 +1,66 @@
+#ifndef  __PrecipitationEffectTest_H__
+#define  __PrecipitationEffectTest_H__
+
+
+#include "gds/Graphics/TextureHandle.hpp"
+#include "gds/Graphics/MeshObjectHandle.hpp"
+#include "gds/Graphics/ShaderHandle.hpp"
+#include "gds/Graphics/Shader/ShaderTechniqueHandle.hpp"
+#include "gds/Input/fwd.hpp"
+
+#include "../../../_Common/GraphicsTestBase.hpp"
+
+
+class CDoubleConeScrollEffect;
+
+
+class CPrecipitationEffectTest : public CGraphicsTestBase
+{
+	boost::shared_ptr<CDoubleConeScrollEffect> m_pDoubleConeScrollEffect;
+
+//	std::vector<MeshHandle> m_vecMesh;
+
+	ShaderHandle m_Shader;
+
+	ShaderTechniqueHandle m_MeshTechnique;
+
+	boost::shared_ptr<FontBase> m_pFont;
+
+	std::string m_TextBuffer;
+
+	TextureHandle m_SkyTexture;
+	MeshHandle m_SkyboxMesh;
+
+private:
+
+	void CreateSampleUI();
+
+	bool InitShader();
+
+	void RenderMeshes();
+
+//	void SaveTexturesAsImageFiles();
+
+public:
+
+	CPrecipitationEffectTest();
+
+	~CPrecipitationEffectTest();
+
+	const char *GetAppTitle() const { return "PrecipitationEffectTest"; }
+
+	int Init();
+
+	void Release() {};
+
+	void Update( float dt );
+
+	void Render();
+
+//	virtual void RenderBase();
+
+	virtual void HandleInput( const InputData& input );
+};
+
+
+#endif /* __PrecipitationEffectTest_H__ */
