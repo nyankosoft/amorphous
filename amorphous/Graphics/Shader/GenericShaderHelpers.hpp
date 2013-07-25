9d29555858da820415660f8c53fbbe0c2816d014
blue <sgt.nyanko@gmail.com>
2013-07-26 00:41:55 +0900

    added: a haeder file which contains a convenience function(s) for creating generic shaders

---
+++ amorphous/Graphics/Shader/GenericShaderHelpers.hpp
@@ -0,0 +1,32 @@
+#ifndef __GenericShaderHelpers_HPP__
+#define __GenericShaderHelpers_HPP__
+
+
+#include "GenericShaderGenerator.hpp"
+#include "amorphous/Support/Log/DefaultLog.hpp"
+
+
+namespace amorphous
+{
+
+
+inline ShaderHandle CreateNoLightingShader()
+{
+	ShaderHandle shader;
+	ShaderResourceDesc desc;
+	GenericShaderDesc gs_desc;
+	gs_desc.Lighting = false;
+	boost::shared_ptr<GenericShaderGenerator> pGenerator( new GenericShaderGenerator(gs_desc) );
+	desc.pShaderGenerator = pGenerator;
+	bool shader_loaded = shader.Load( desc );
+	if( !shader_loaded )
+		LOG_PRINT_ERROR( " Failed to load the shader." );
+
+	return shader;
+}
+
+
+} // namespace amorphous
+
+
+#endif /* __GenericShaderHelpers_HPP__ */
