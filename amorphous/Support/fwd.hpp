de0fb78dae021fbef6c67af920f7f201574ab966
wanko <amorphous.git@gmail.com>
2013-07-31 23:00:19 +0900

    updated: amorphous/Support/fwd.hpp
    added: BitmapImage.
    Also added #ifndef/#define/#endif directives.

--- amorphous/Support/fwd.hpp
+++ amorphous/Support/fwd.hpp
@@ -1,7 +1,15 @@
+#ifndef __FWD_Support_HPP__
+#define __FWD_Support_HPP__
+
+
 namespace amorphous
 {
 
 class stream_buffer;
 class Timer;
+class BitmapImage;
 
 } // namespace amorphous
+
+
+#endif /* __FWD_Support_HPP__ */
