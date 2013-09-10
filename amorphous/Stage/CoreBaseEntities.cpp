b882fdc4a25cd169ccf55142451c8644796127a6
wanko <amorphous.git@gmail.com>
2013-09-10 23:34:59 +0900

    deleted: unnecessary header inclusions

--- amorphous/Stage/CoreBaseEntities.cpp
+++ amorphous/Stage/CoreBaseEntities.cpp
@@ -1,6 +1,5 @@
 #include "CoreBaseEntitiesLoader.hpp"
 #include "BaseEntityFactory.hpp"
-#include "BaseEntity.hpp"
 #include "BE_AreaSensor.hpp"
 #include "BE_Blast.hpp"
 #include "BE_Bullet.hpp"
@@ -8,41 +7,28 @@
 #include "BE_Decal.hpp"
 #include "BE_Door.hpp"
 #include "BE_DoorController.hpp"
-//#include "BE_Enemy.hpp"
 #include "BE_EventTrigger.hpp"
 #include "BE_ExplosionSmoke.hpp"
 #include "BE_Explosive.hpp"
-#include "BE_FixedTurretPod.hpp"
-//#include "BE_Floater.hpp"
 #include "BE_GeneralEntity.hpp"
 #include "BE_HomingMissile.hpp"
 #include "BE_LaserDot.hpp"
 #include "BE_MuzzleFlash.hpp"
 #include "BE_ParticleSet.hpp"
 #include "BE_PhysicsBaseEntity.hpp"
-#include "BE_Platform.hpp"
-#include "BE_Player.hpp"
-
-
-//#include "BE_PlayerPseudoAircraft.hpp"		// 20061119
-//#include "BE_PlayerPseudoLeggedVehicle.hpp"
-//#include "BE_PlayerShip.hpp"
 #include "BE_PointLight.hpp"
 #include "BE_SmokeTrace.hpp"
 #include "BE_SupplyItem.hpp"
 #include "BE_TextureAnimation.hpp"
-#include "BE_Turret.hpp"
-//#include "BE_EnemyAircraft.hpp"    // 20070106
-#include "BE_DirectionalLight.hpp"   // 20070108
-#include "BE_StaticParticleSet.hpp"  // 20070328
-#include "BE_NozzleExhaust.hpp"      // 20070328
-//#include "BE_EnemyShip.hpp"        // 20070520
-#include "BE_StaticGeometry.hpp"     // 20070817
-#include "BE_Skybox.hpp"             // 20070817
-#include "BE_CameraController.hpp"   // 20070909
-#include "BE_ScriptedCamera.hpp"     // 20070909
-#include "BE_IndividualEntity.hpp"   // 20070909
-#include "BE_StaticLiquid.hpp"       // 20110418
+#include "BE_DirectionalLight.hpp"
+#include "BE_StaticParticleSet.hpp"
+#include "BE_NozzleExhaust.hpp"
+#include "BE_StaticGeometry.hpp"
+#include "BE_Skybox.hpp"
+#include "BE_CameraController.hpp"
+#include "BE_ScriptedCamera.hpp"
+#include "BE_IndividualEntity.hpp"
+#include "BE_StaticLiquid.hpp"
 
 
 namespace amorphous
