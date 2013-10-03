c4ca178c4a0dce304e935403c389678bd8fe77ff
wanko <amorphous.git@gmail.com>
2013-10-04 00:40:19 +0900

    changed: MouseCursor
    It now uses a graphics element to render the mouse cursor instead of C2DRect.

--- amorphous/GameCommon/MouseCursor.cpp
+++ amorphous/GameCommon/MouseCursor.cpp
@@ -1,4 +1,5 @@
 #include "MouseCursor.hpp"
+#include "amorphous/Graphics/GraphicsElementManager.hpp"
 
 
 namespace amorphous
@@ -6,6 +7,64 @@ namespace amorphous
 
 
 singleton<MouseCursor> MouseCursor::m_obj;
+
+
+void MouseCursor::Show()
+{
+	m_bDisplay = true;
+
+	if( m_pCursor )
+		m_pCursor->SetColor( 1, SFloatRGBAColor(1,1,1,1) );
+}
+
+
+void MouseCursor::Hide()
+{
+	m_bDisplay = false;
+
+	if( m_pCursor )
+		m_pCursor->SetColor( 1, SFloatRGBAColor(0,0,0,0) );
+}
+
+
+void MouseCursor::Load( GraphicsElementManager& mgr, int index, const SRect& rect, const std::string& tex_filename, const SFloatRGBAColor& color )
+{
+	int layer = 0;
+
+	m_pCursor = mgr.CreateFillRect(
+		rect,
+		color,
+		layer
+		);
+}
+
+
+void MouseCursor::UpdateCursorPosition( int x, int y )
+{
+	m_PosX = x;
+	m_PosY = y;
+
+	if( m_pCursor )
+		m_pCursor->SetLocalTopLeftPos( SPoint(x,y) );
+}
+
+
+void MouseCursor::CreateDefault( GraphicsElementManager& mgr )
+{
+	Vector2 v0( 0, 0);
+	Vector2 v1(20,15);
+	Vector2 v2(15,20);
+
+	m_pCursor = mgr.CreateFillTriangle( v0, v1, v2, SFloatRGBAColor::White() );
+}
+
+
+void MouseCursor::ReleaseGraphicsResources( GraphicsElementManager& mgr )
+{
+	bool res = mgr.RemoveElement( m_pCursor );
+
+	// Release texture(s) registered to the graphics element manager?
+}
 
 
 } // namespace amorphous
--- amorphous/GameCommon/MouseCursor.hpp
+++ amorphous/GameCommon/MouseCursor.hpp
@@ -3,9 +3,8 @@
 
 
 #include "amorphous/Support/Singleton.hpp"
-#include "amorphous/base.hpp"
-#include "amorphous/Graphics/2DPrimitive/2DRect.hpp"
-#include "amorphous/Graphics/TextureHandle.hpp"
+#include "amorphous/Graphics/fwd.hpp"
+#include "amorphous/Graphics/FloatRGBAColor.hpp"
 #include "amorphous/Graphics/Rect.hpp"
 #include <string>
 
@@ -18,8 +17,7 @@ class MouseCursor
 {
 	static singleton<MouseCursor> m_obj;
 
-	TextureHandle m_Texture[4];
-	C2DRect m_Rect[4];
+	boost::shared_ptr<GraphicsElement> m_pCursor;
 
 	bool m_bDisplay;
 
@@ -31,53 +29,24 @@ public:
 
 	static MouseCursor* Get() { return m_obj.get(); }
 
-	MouseCursor() : m_CurrentCursorIndex(0), m_bDisplay(true), m_PosX(0), m_PosY(0) { m_Rect[0] = C2DRect(0,0,0,0,0xFFFFFFFF); }
+	MouseCursor() : m_CurrentCursorIndex(0), m_bDisplay(true), m_PosX(0), m_PosY(0) {}// m_Rect[0] = C2DRect(0,0,0,0,0xFFFFFFFF); }
 
-	inline void Show()	{ m_bDisplay = true; }
-	inline void Hide()	{ m_bDisplay = false; }
+	void Show();
 
-	void Load( int index, const SRect& rect, const std::string& tex_filename, U32 color = 0xFFFFFFFF )
+	void Hide();
+
+	void CreateDefault( GraphicsElementManager& mgr );
+
+	void Load( GraphicsElementManager& mgr, int index, const SRect& rect, const std::string& tex_filename, const SFloatRGBAColor& color = SFloatRGBAColor::White() );
+
+	void Load( GraphicsElementManager& mgr, int index, int l, int t, int r, int b, const std::string& tex_filename, const SFloatRGBAColor& color = SFloatRGBAColor::White() )
 	{
-		Load( index, rect.left, rect.top, rect.right, rect.bottom, tex_filename, color );
+		Load( mgr, index, l, t, r, b, tex_filename, color );
 	}
 
-	void Load( int index, int l, int t, int r, int b, const std::string& tex_filename, U32 color = 0xFFFFFFFF )
-	{
-		if( index < 0 || 4 <= index )
-			return;
+	void UpdateCursorPosition( int x, int y );
 
-		m_Rect[index] = C2DRect( l, t, r, b, color );
-		m_Texture[index].Load( tex_filename );
-
-		// use the entire region of the texture for cursor rect
-		m_Rect[index].SetTextureUV( TEXCOORD2(0,0), TEXCOORD2(1,1) );
-	}
-
-	void UpdateCursorPosition( int x, int y )
-	{
-		m_PosX = x;
-		m_PosY = y;
-	}
-
-	void Draw()
-	{
-		if( !m_bDisplay )
-			return;
-
-		C2DRect& src_rect = m_Rect[m_CurrentCursorIndex];
-
-		C2DRect offset_rect = src_rect;
-		offset_rect.Translate( m_PosX, m_PosY );
-		offset_rect.Draw( m_Texture[m_CurrentCursorIndex] );
-	}
-
-	void ReleaseGraphicsResources()
-	{
-		for( int i=0; i<4; i++ )
-		{
-			m_Texture[i].Release();
-		}
-	}
+	void ReleaseGraphicsResources( GraphicsElementManager& mgr );
 };
 
 
