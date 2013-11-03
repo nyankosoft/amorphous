656d380b78d1e3e4b73e673276cc2b03fced7df7
wanko <amorphous.git@gmail.com>
2013-11-03 22:32:30 +0900

    cleaned up: the source code

--- amorphous/Support/DebugOutput.cpp
+++ amorphous/Support/DebugOutput.cpp
@@ -1,11 +1,6 @@
 #include "DebugOutput.hpp"
 
-#include "Graphics/2DPrimitive/2DRect.hpp"
 #include "Graphics/LogOutput_OnScreen.hpp"
-#include "Graphics/GraphicsResourceManager.hpp"
-#include "Sound/SoundManager.hpp"
-#include "Input/InputDevice.hpp"
-
 #include "Support/memory_helpers.hpp"
 #include "Support/StringAux.hpp"
 #include "Support/Profile.hpp"
@@ -28,28 +23,13 @@ m_pLogOutput(pLogOutput)
 
 void DebugInfo_Log::UpdateDebugInfoText()
 {
-/*	FontBase *pFont = m_pFont;
-
-	const int font_width  = pFont->GetFontWidth();
-	const int font_height = pFont->GetFontHeight();*/
-//	const int num_rows = ???;
 	const int num_current_rows = m_pLogOutput->GetNumCurrentRows();
 
-	// draw rect for background
-//	const int sx = (int)m_vTopLeftPos.x;
-//	const int sy = (int)m_vTopLeftPos.y;
-//	C2DRect bg_rect( sx, sy, sx + font_width * 80/*m_NumChars*/, sy + font_height * num_current_rows, 0x80000000 );
-//	bg_rect.Draw();
-
-
-//	Vector2 vTextPos = m_vTopLeftPos;
 	SFloatRGBAColor color;
-	for( int i=0; i<num_current_rows; i++ )//, vTextPos.y += font_height )
+	for( int i=0; i<num_current_rows; i++ )
 	{
 		const char *pText = m_pLogOutput->GetText( i );
 		const U32 argb_color   = m_pLogOutput->GetTextColor( i );
-
-//		pFont->DrawText( pText, vTextPos, color );
 
 		color.SetARGB32( argb_color );
 		m_MultiLineTextBuffer.push_back( pText );
@@ -73,25 +53,10 @@ void DebugInfo_Profile::UpdateDebugInfoText()
 			num_max_chars = rvecProfileText[i].length();
 	}
 
-	// draw a background rect
-//	int font_width, font_height;
-//	m_pFont->GetFontSize( font_width, font_height );
-
-//	const Vector2 vLeftTop = m_vTopLeftPos;
-//	C2DRect rect( 
-//		vLeftTop,
-//		vLeftTop + Vector2( (float)(font_width * (num_max_chars + 2)), (float)(font_height * num_lines) ),
-//		0x80000000 );
-//	rect.Draw();
-
-	// draw text
-//	Vector2 vPos = m_vTopLeftPos;
-//	FontBase *pFont = m_pFont;
+	// Store text to the buffer
 	m_MultiLineTextBuffer.resize( 0 );
 	for( i=0; i<num_lines; i++ )
 	{
-//		pFont->DrawText( rvecProfileText[i].c_str(), vPos, 0xFFFFFFFF );
-//		vPos.y += font_height;
 		m_MultiLineTextBuffer.push_back( rvecProfileText[i] );
 	}
 }
@@ -99,17 +64,10 @@ void DebugInfo_Profile::UpdateDebugInfoText()
 
 void DebugInfo_StateLog::UpdateDebugInfoText()
 {
-//	FontBase* pFont = m_pFont;
-
-//	float fLetterHeight = (float)pFont->GetFontHeight();
-//	Vector2 v2d = m_vTopLeftPos;
-
 	m_MultiLineTextBuffer.resize( 0 );
 	int row, num_rows = StateLog.GetNumLogs();
 	for( row=2; row<num_rows; row++ )
 	{
-//		pFont->DrawText( StateLog.GetLog(row).c_str(), v2d );
-//		v2d.y += fLetterHeight;
 		m_MultiLineTextBuffer.push_back( StateLog.GetLog(row) );
 	}
 }
