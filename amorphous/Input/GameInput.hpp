b3bd7fe617ee2d14d8fcd40be2837314930b0dfc
wanko <amorphous.git@gmail.com>
2013-11-12 00:54:50 +0900

    deleted: an outdated header file

--- amorphous/Input/GameInput.hpp
+++
@@ -1,8 +0,0 @@
-#include "InputHub.hpp"
-#include "InputHandler.hpp"
-#include "3DActionCode.hpp"
-
-#include "DirectInput.hpp"
-#include "DIKeyboard.hpp"
-#include "DirectInputMouse.hpp"
-#include "DirectInputGamepad.hpp"
