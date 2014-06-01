a7b0dca467aa889d25e05523d98d85bc30255cf9
wanko <amorphous.git@gmail.com>
2014-06-01 21:36:36 +0900

    added: comments

--- framework/amorphous/Graphics/2DPrimitive/2DPrimitive.cpp
+++ framework/amorphous/Graphics/2DPrimitive/2DPrimitive.cpp
@@ -1,6 +1,24 @@
 #include "2DPrimitive.hpp"
 
 
+/*
+2D primitive classes
+
+Class                    # vertices    # triangles     Primitive types      Parent class
+-----------------------------------------------------------------------------------------
+C2DTriangle               3            1               TRIANGLE_LIST        C2DPrimitive
+C2DRect                   4            2               TRIANGLE_FAN         C2DPrimitive
+C2DFrameRect             10            8               TRIANGLE_STRIP       C2DPrimitive
+C2DRoundRect             Variable      Variable        TRIANGLE_FAN         C2DPrimitive
+C2DRoundFrameRect        Variable      Variable        TRIANGLE_STRIP       C2DRoundRect
+C2DRegularPolygon        Variable      Variable        TRIANGLE_STRIP       C2DRoundRect
+
+
+
+
+*/
+
+
 namespace amorphous
 {
 
