db9be418b82d3bafa780f32e4fd133b639631e3d
wanko <amorphous.git@gmail.com>
2014-08-02 00:22:54 +0900

    updated: Run()
    OpenGL is selected as the default graphics library.

--- demo/_Common/TestMain_Win32.cpp
+++ demo/_Common/TestMain_Win32.cpp
@@ -361,11 +361,11 @@ static int Run( LPSTR lpCmdLine )
 //	CXMLParserInitReleaseManager parser_mgr;
 */
 
-	string graphics_library_name = "Direct3D";
+	string graphics_library_name = "OpenGL";
 	ParamLoader param_loader( "config" );
 	if( param_loader.IsReady() )
 	{
-		param_loader.LoadParam( "GraphicsLibrary", graphics_library_name );
+		param_loader.LoadParam( "graphics_library", graphics_library_name );
 	}
 
 	Result::Name res = SelectGraphicsLibrary( graphics_library_name );
