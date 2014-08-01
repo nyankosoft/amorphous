92fff2d827e4da5b408588245f962737a8e9b915
wanko <amorphous.git@gmail.com>
2014-08-02 00:15:39 +0900

    updated: Profile.cpp

--- framework/amorphous/Support/Profile.cpp
+++ framework/amorphous/Support/Profile.cpp
@@ -16,7 +16,6 @@
 #include <stdio.h>
 #include <boost/shared_ptr.hpp>
 
-#include "SafeDelete.hpp"
 #include "Support/Timer.hpp"
 
 
@@ -45,27 +44,33 @@ public:
 };
 
 
-typedef struct
+class ProfileSample
 {
-   bool bValid;						///< Whether this data is valid
-   unsigned int iProfileInstances;	///< # of times ProfileBegin called
-   int iOpenProfiles;				///< # of times ProfileBegin w/o ProfileEnd
-   char szName[256];				///< Name of sample
-   float fStartTime;				///< The current open profile start time
-   float fAccumulator;				///< All samples this frame added together
-   float fChildrenSampleTime;		///< Time taken by all children
-   unsigned int iNumParents;		///< Number of profile parents
-} ProfileSample;
+public:
+	bool bValid;					///< Whether this data is valid
+	unsigned int iProfileInstances;	///< # of times ProfileBegin called
+	int iOpenProfiles;				///< # of times ProfileBegin w/o ProfileEnd
+	char szName[256];				///< Name of sample
+	float fStartTime;				///< The current open profile start time
+	float fAccumulator;				///< All samples this frame added together
+	float fChildrenSampleTime;		///< Time taken by all children
+	unsigned int iNumParents;		///< Number of profile parents
+
+	ProfileSample() : bValid(false), iProfileInstances(0), iOpenProfiles(0), fStartTime(0), fAccumulator(0), fChildrenSampleTime(0), iNumParents(0) {}
+};
 
 
-typedef struct
+class ProfileSampleHistory
 {
-   bool bValid;        ///< Whether the data is valid
-   char szName[256];   ///< Name of the sample
-   float fAve;         ///< Average time per frame (percentage)
-   float fMin;         ///< Minimum time per frame (percentage)
-   float fMax;         ///< Maximum time per frame (percentage)
-} ProfileSampleHistory;
+public:
+	bool bValid;        ///< Whether the data is valid
+	char szName[256];   ///< Name of the sample
+	float fAve;         ///< Average time per frame (percentage)
+	float fMin;         ///< Minimum time per frame (percentage)
+	float fMax;         ///< Maximum time per frame (percentage)
+
+	ProfileSampleHistory() : bValid(false), fAve(0), fMin(0), fMax(0) {}
+};
 
 
 
