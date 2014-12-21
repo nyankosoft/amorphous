df3c2c0b44ae0828356bc686c4033124183c61fc
wanko <amorphous.git@gmail.com>
2014-12-21 12:04:32 +0900

    changed: CGraphicsTestBase
    Screenshots are now taken and saved by GraphicsApplicationbase.

--- demo/_Common/GraphicsTestBase.cpp
+++ demo/_Common/GraphicsTestBase.cpp
@@ -14,114 +14,6 @@
 static Camera sg_Camera;
 
 
-class ScreenshotRenderer
-{
-public:
-	virtual ~ScreenshotRenderer(){}
-
-	virtual void Render() = 0;
-};
-
-
-template<class Renderer>
-class ScreenshotRendererDelegate : public ScreenshotRenderer
-{
-	Renderer& m_Renderer;
-public:
-
-	ScreenshotRendererDelegate(Renderer& renderer)
-		:
-	m_Renderer(renderer)
-	{}
-
-	void Render()
-	{
-		m_Renderer.Render();
-	}
-};
-
-
-unsigned int GetNextImageFileNumber( const std::string& directory_path, const std::string& basename )
-{
-	using namespace boost::filesystem;
-
-	if( !exists(directory_path) )
-		return 0;
-	
-	unsigned int count = 0;
-
-	boost::system::error_code ec;
-	directory_iterator itr( directory_path, ec );
-
-	if( ec != boost::system::errc::success )
-		return 0;
-
-	directory_iterator end_itr; // default construction yields past-the-end
-	for( ; itr != end_itr; ++itr )
-	{
-		const path& pathname = *itr;
-
-		if( pathname.leaf().string().find( basename ) == 0 )
-			count += 1;
-	}
-
-	return count;
-}
-
-
-/// \param image_extension Starts with '.', e.g. ".png"
-void TakeScreenshot(
-	ScreenshotRenderer& renderer,
-	const std::string& directory_path,
-	const std::string& basename,
-	const char *image_extension = ".png"
-	)
-{
-	using namespace boost::filesystem;
-
-	boost::shared_ptr<TextureRenderTarget> pTextureRenderTarget = TextureRenderTarget::Create();
-	const int screen_width  = GetGameWindowManager().GetScreenWidth();
-	const int screen_height = GetGameWindowManager().GetScreenHeight();
-	pTextureRenderTarget->Init( screen_width, screen_height );
-	pTextureRenderTarget->SetRenderTarget();
-
-	renderer.Render();
-
-	if( !exists(directory_path) )
-	{
-		boost::system::error_code ec;
-		create_directories( directory_path, ec );
-		if( ec.value() != 0 )
-		{
-			LOG_PRINT_ERROR( "create_directories() failed (error: " + ec.message() + ")." );
-			return;
-		}
-	}
-
-	pTextureRenderTarget->ResetRenderTarget();
-	unsigned int count = GetNextImageFileNumber( directory_path, basename );
-	path directory_path_and_basename = path(directory_path) / path(basename);
-//	std::string img_file_path = fmt_string( "screenshots/image%04d", count ) + "." + img_ext;
-	std::string img_file_pathname
-		= fmt_string( "%s%04d", directory_path_and_basename.string().c_str(), count ) + image_extension;
-
-	pTextureRenderTarget->GetRenderTargetTexture().SaveTextureToImageFile( img_file_pathname );
-}
-
-
-//template<class T>
-//void TakeScreenshot(
-//	T& renderer_class_object,
-//	const std::string& directory_path,
-//	const std::string& basename,
-//	const char *image_extension = ".png"
-//	)
-//{
-//	ScreenshotRendererDelegate<T> renderer(renderer_class_object);
-//	TakeScreenshot( renderer, directory_path, basename, image_extension );
-//}
-
-
 int CGraphicsTestBase::ms_CameraControllerInputHandlerIndex = 1;
 
 
@@ -193,12 +85,6 @@ void CGraphicsTestBase::HandleInput( const InputData& input )
 	case GIC_F12:
 		if( input.iType == ITYPE_KEY_PRESSED )
 		{
-			ScreenshotRendererDelegate<CGraphicsTestBase> renderer(*this);
-			TakeScreenshot(
-				renderer,
-				"screenshots",
-				"graphics_demo"
-				);
 		}
 		break;
 	default:
