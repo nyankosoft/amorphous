d977b72ca02b6403ac9d794934e291e97a904e8d
wanko <amorphous.git@gmail.com>
2014-12-24 00:20:30 +0900

    added: EmbeddedMiscShader

---
+++ framework/amorphous/Graphics/Shader/Embedded/EmbeddedMiscShader.hpp
@@ -0,0 +1,39 @@
+#ifndef __EmbeddedMiscShader_HPP__
+#define __EmbeddedMiscShader_HPP__
+
+
+#include <string>
+#include <boost/shared_ptr.hpp>
+#include "amorphous/base.hpp"
+#include "amorphous/Graphics/fwd.hpp"
+
+
+namespace amorphous
+{
+
+
+class EmbeddedMiscShader
+{
+public:
+
+	EmbeddedMiscShader() {}
+
+	virtual ~EmbeddedMiscShader(){}
+
+	// Derived classes must implement either
+	// 1. GenerateShader()
+	// or
+	// 2. GenerateVertexShader() & GenerateFragmentShader()
+
+	virtual Result::Name GenerateShader( const GenericShaderDesc& desc, std::string& shader ) { return Result::UNKNOWN_ERROR; }
+
+	virtual Result::Name GenerateVertexShader( const GenericShaderDesc& desc, std::string& shader ) { return Result::UNKNOWN_ERROR; }
+
+	virtual Result::Name GenerateFragmentShader( const GenericShaderDesc& desc, std::string& shader ) { return Result::UNKNOWN_ERROR; }
+};
+
+
+} // namespace amorphous
+
+
+#endif /* __EmbeddedMiscShader_HPP__ */
