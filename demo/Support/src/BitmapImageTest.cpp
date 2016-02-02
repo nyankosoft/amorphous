64ed8d361760af2e66ed84f68837dc4b74604b06
wanko <amorphous.git@gmail.com>
2016-02-03 00:44:20 +0900

    updated: the test of BitmapImage

--- demo/Support/src/BitmapImageTest.cpp
+++ demo/Support/src/BitmapImageTest.cpp
@@ -7,6 +7,10 @@ using namespace std;
 using namespace amorphous;
 
 
+// Resolved boost link error by doing this:
+// http://stackoverflow.com/questions/33815603/linker-error-boost-chrono-to-boost-timer
+
+
 void LoadImageForTest( const std::string& image_filepath )
 {
 	LOG_SCOPE( image_filepath.c_str() );
@@ -91,13 +95,13 @@ void RunImageSaveTest()
 				BitmapImage img64( 64, 64, bpps[j] );
 				img64.FillColor( colors[k] );
 
-				image_file_pathname = string("results/bitmap_images/") + string(basenames[k]) + "_064x064_bpp" + to_string(bpps[j]) + "." + string(extensions[i]);
+				image_file_pathname = string("results/bitmap_images/") + string(basenames[k]) + "_064x064_bpp" + std::to_string(bpps[j]) + "." + string(extensions[i]);
 				img64.SaveToFile( image_file_pathname );
 
 				BitmapImage img256( 256, 256, bpps[j] );
 				img256.FillColor( colors[k] );
 
-				image_file_pathname = string("results/bitmap_images/") + string(basenames[k]) + "_256x256_bpp" + to_string(bpps[j]) + "." + string(extensions[i]);
+				image_file_pathname = string("results/bitmap_images/") + string(basenames[k]) + "_256x256_bpp" + std::to_string(bpps[j]) + "." + string(extensions[i]);
 				img256.SaveToFile( image_file_pathname );
 			}
 		}
@@ -105,9 +109,77 @@ void RunImageSaveTest()
 }
 
 
+void RunPixelOperationsTest( BitmapImage& img )
+{
+	LOG_PRINTF(("bpp: %d", img.GetFBITMAP() ? FreeImage_GetBPP(img.GetFBITMAP()) : 0));
+
+	int w = img.GetWidth();
+	int h = img.GetHeight();
+	U8 r=0,g=0,b=0,a=0;
+	img.GetPixel(0,0,r,g,b);
+	LOG_PRINTF(("pixel(0,0): RGB = %u, %u, %u",r,g,b));
+	r=0,g=0,b=0,a=0;
+	img.GetPixel(0,0,r,g,b,a);
+	LOG_PRINTF(("pixel(0,0): RGBA = %u, %u, %u, %u",r,g,b,a));
+
+	r=0,g=0,b=0,a=0;
+	img.GetPixel(w-1,0,r,g,b);
+	LOG_PRINTF(("pixel(w-1,0): RGB = %u, %u, %u",r,g,b));
+	r=0,g=0,b=0,a=0;
+	img.GetPixel(w-1,0,r,g,b,a);
+	LOG_PRINTF(("pixel(w-1,0): RGBA = %u, %u, %u, %u",r,g,b,a));
+
+	r=0,g=0,b=0,a=0;
+	img.GetPixel(0,h-1,r,g,b);
+	LOG_PRINTF(("pixel(0,h-1): RGB = %u, %u, %u",r,g,b));
+	r=0,g=0,b=0,a=0;
+	img.GetPixel(0,h-1,r,g,b,a);
+	LOG_PRINTF(("pixel(0,h-1): RGBA = %u, %u, %u, %u",r,g,b,a));
+
+	r=0,g=0,b=0,a=0;
+	img.GetPixel(w-1,h-1,r,g,b);
+	LOG_PRINTF(("pixel(w-1,h-1): RGB = %u, %u, %u",r,g,b));
+	r=0,g=0,b=0,a=0;
+	img.GetPixel(w-1,h-1,r,g,b,a);
+	LOG_PRINTF(("pixel(w-1,h-1): RGBA = %u, %u, %u, %u",r,g,b,a));
+
+	r=255; g=255; b=0; a=255;
+	img.SetPixel(0,  0,r,g,b);
+	r=0; g=0; b=255; a=255;
+	img.SetPixel(w-1,0,r,g,b,a);
+	r=0; g=255; b=0; a=255;
+	img.SetPixel(0,  h-1,r,g,b);
+	r=255; g=0; b=0; a=255;
+	img.SetPixel(w-1,h-1,r,g,b,a);
+}
+
+
+void RunPixelOperationsTest()
+{
+	BitmapImage png;
+	bool loaded = png.LoadFromFile("images/get_pixel_test.png");
+
+	LOG_PRINT("PNG:");
+	RunPixelOperationsTest( png );
+
+	png.SaveToFile("images/set_pixel_test.png");
+
+
+	BitmapImage jpg;
+	loaded = jpg.LoadFromFile("images/get_pixel_test.jpg");
+
+	LOG_PRINT("JPG:");
+	RunPixelOperationsTest( jpg );
+
+	jpg.SaveToFile("images/set_pixel_test.jpg");
+}
+
+
 int RunTests()
 {
-	RunImageSaveTest();
+//	RunImageSaveTest();
+
+	RunPixelOperationsTest();
 
 //	TestImageArchive();
 
