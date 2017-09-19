f22921fd5e920d0c0ecd5940738f9d19c1e013ff
Takashi Kumagai <amorphous.git@gmail.com>
2017-09-19 16:51:03 +0900

    Deboosted fixed width integer types.

--- framework/amorphous/base.hpp
+++ framework/amorphous/base.hpp
@@ -16,17 +16,17 @@ typedef unsigned int   uint;
 typedef unsigned long  ulong;
 
 
-#include <boost/integer.hpp>
+#include <cstdint>
 
-typedef boost::uint8_t  U8;
-typedef boost::uint16_t U16;
-typedef boost::uint32_t U32;
-typedef boost::uint64_t U64;
+typedef uint8_t  U8;
+typedef uint16_t U16;
+typedef uint32_t U32;
+typedef uint64_t U64;
 
-typedef boost::int8_t  I8;
-typedef boost::int16_t I16;
-typedef boost::int32_t I32;
-typedef boost::int64_t I64;
+typedef int8_t  I8;
+typedef int16_t I16;
+typedef int32_t I32;
+typedef int64_t I64;
 
 
 //
