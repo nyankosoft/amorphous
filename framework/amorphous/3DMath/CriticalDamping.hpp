417f552ca6061159f7a019ce9a00de23ca936d29
Takashi Kumagai <amorphous.git@gmail.com>
2017-10-07 13:06:41 +0900

    Commented out one of two SmoothCD() overloads. The one that was commented out required the presence of a global variable and caused a compile error with the Android NDK compiler.

--- framework/amorphous/3DMath/CriticalDamping.hpp
+++ framework/amorphous/3DMath/CriticalDamping.hpp
@@ -10,17 +10,17 @@
 //       where it expects to find a value for delta-t.
 //
 
-template <typename T>
-inline T SmoothCD(const T &from, const T &to, T &vel, float smoothTime)
-{
-	float omega = 2.f/smoothTime;
-	float x = omega*timeDelta;
-	float exp = 1.f/(1.f+x+0.48f*x*x+0.235f*x*x*x);
-	T change = from - to;
-	T temp = (vel+omega*change)*timeDelta;
-	vel = (vel - omega*temp)*exp;  // Equation 5
-	return to + (change+temp)*exp; // Equation 4
-}
+//template <typename T>
+//inline T SmoothCD(const T &from, const T &to, T &vel, float smoothTime)
+//{
+//	float omega = 2.f/smoothTime;
+//	float x = omega*timeDelta;
+//	float exp = 1.f/(1.f+x+0.48f*x*x+0.235f*x*x*x);
+//	T change = from - to;
+//	T temp = (vel+omega*change)*timeDelta;
+//	vel = (vel - omega*temp)*exp;  // Equation 5
+//	return to + (change+temp)*exp; // Equation 4
+//}
 
 
 
