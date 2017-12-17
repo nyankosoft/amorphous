90c64a2f53a078dae320c48abbd70ce36494bc32
nyankosoft <sgt.nyanko@gmail.com>
2017-12-17 19:30:02 +0900

    Added CreateHSDirectionalLightingShader().

--- framework/amorphous/Graphics/Shader/GenericShaderHelpers.hpp
+++ framework/amorphous/Graphics/Shader/GenericShaderHelpers.hpp
@@ -5,6 +5,11 @@
 #include "GenericShaderGenerator.hpp"
 #include "amorphous/Support/Log/DefaultLog.hpp"
 
+// Helper function here are similar to those defined in CommonShaders.hpp,
+// so perhaps they should be merged into one.
+// The differences are that functions below create and returns a new shader handle
+// whoere ones in CommonShaders.hpp returns a static reference to shader handles.
+
 
 namespace amorphous
 {
@@ -23,6 +28,23 @@ inline ShaderHandle CreateNoLightingShader()
 		LOG_PRINT_ERROR( " Failed to load the shader." );
 
 	return shader;
+}
+
+
+inline ShaderHandle CreateHSDirectionalLightingShader()
+{
+	ShaderHandle shader;
+	ShaderResourceDesc desc;
+	GenericShaderDesc gs_desc;
+	gs_desc.NumDirectionalLights = 1;
+	std::shared_ptr<GenericShaderGenerator> pGenerator( new GenericShaderGenerator(gs_desc) );
+	desc.pShaderGenerator = pGenerator;
+	bool shader_loaded = shader.Load( desc );
+	if( !shader_loaded )
+		LOG_PRINT_ERROR( " Failed to load a shader (hs dl)." );
+
+	return shader;
+	
 }
 
 
